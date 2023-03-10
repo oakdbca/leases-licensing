43265d9bb8f6e3454a447591df750cc8f4941402
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-10 16:53:36 +0800

    Added person lookup api.

--- leaseslicensing/components/users/api.py
+++ leaseslicensing/components/users/api.py
@@ -1,40 +1,40 @@
-import traceback
-from django.db import transaction
-from django.db.models import Q
 from django.conf import settings
-from django_countries import countries
-from rest_framework import viewsets, serializers, status, generics, views
-from rest_framework.decorators import action as detail_route, renderer_classes
-from rest_framework.decorators import action as list_route
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
 from django.core.cache import cache
-from ledger_api_client.ledger_models import (
-    EmailUserRO as EmailUser,
-    Address,
-    EmailIdentity,
-)  # EmailUserAction
+from django.db import transaction
+from django.db.models import CharField, Q, Value
+from django.db.models.functions import Concat
+from django_countries import countries
+from ledger_api_client.ledger_models import Address
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser  # EmailUserAction
+from rest_framework import filters, generics, views, viewsets
+from rest_framework.decorators import action
+from rest_framework.decorators import action as detail_route
+from rest_framework.decorators import action as list_route
+from rest_framework.decorators import renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from leaseslicensing.components.invoicing.models import ChargeMethod, RepetitionType
-from leaseslicensing.components.invoicing.serializers import ChargeMethodSerializer, RepetitionTypeSerializer
-# from leaseslicensing.components.main.utils import retrieve_department_users
-from leaseslicensing.components.main.decorators import basic_exception_handler
-from leaseslicensing.components.main.serializers import EmailUserSerializer
-from leaseslicensing.components.users.serializers import (
-    UserSerializer,
-    UserFilterSerializer,
-    UserAddressSerializer,
-    PersonalSerializer,
-    ContactSerializer,
-    UserSystemSettingsSerializer,
+from leaseslicensing.components.invoicing.serializers import (
+    ChargeMethodSerializer,
+    RepetitionTypeSerializer,
 )
+from leaseslicensing.components.main.decorators import basic_exception_handler
+from leaseslicensing.components.main.models import UserSystemSettings
+from leaseslicensing.components.main.serializers import EmailUserSerializer
 from leaseslicensing.components.organisations.serializers import (
     OrganisationRequestDTSerializer,
 )
-from leaseslicensing.components.main.models import UserSystemSettings
+from leaseslicensing.components.users.serializers import (
+    ContactSerializer,
+    PersonalSerializer,
+    UserAddressSerializer,
+    UserFilterSerializer,
+    UserSerializer,
+    UserSystemSettingsSerializer,
+)
 
-
-#class DepartmentUserList(views.APIView):
+# class DepartmentUserList(views.APIView):
 #    renderer_classes = [
 #        JSONRenderer,
 #    ]
@@ -96,9 +96,6 @@ class GetProfile(views.APIView):
         return Response(serializer.data)
 
 
-from rest_framework import filters
-
-
 class UserListFilterView(generics.ListAPIView):
     """https://cop-internal.dbca.wa.gov.au/api/filtered_users?search=russell"""
 
@@ -114,22 +111,80 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @basic_exception_handler
     def list(self, request, *args, **kwargs):
-        search_term = request.GET.get('term', '')
-        queryset = self.queryset.filter(Q(first_name__icontains=search_term) | Q(last_name__icontains=search_term))[:10]
+        search_term = request.GET.get("term", "")
+        queryset = self.queryset.filter(
+            Q(first_name__icontains=search_term) | Q(last_name__icontains=search_term)
+        )[:10]
         data_transform = EmailUserSerializer(queryset, many=True)
         return Response(data_transform.data)
 
-    @list_route(methods=['GET',], detail=False)
-    @basic_exception_handler
-    def get_department_users(self, request, *args, **kwargs):
-        search_term = request.GET.get('term', '')
-        data = EmailUser.objects.filter(is_staff=True). \
-            filter(Q(first_name__icontains=search_term) | Q(last_name__icontains=search_term)). \
-            values('email', 'first_name', 'last_name')[:10]
-        data_transform = [{'id': person['email'], 'text': person['first_name'] + ' ' + person['last_name']} for person in data]
+    @action(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+    )
+    def person_lookup(self, request, *args, **kwargs):
+        search_term = request.GET.get("term", "")
+        people = self.get_queryset().annotate(
+            search_term=Concat(
+                "first_name",
+                Value(" "),
+                "last_name",
+                Value(" "),
+                "email",
+                output_field=CharField(),
+            )
+        )
+        people = people.filter(search_term__icontains=search_term).values(
+            "id", "email", "first_name", "last_name"
+        )[:10]
+        data_transform = [
+            {
+                "id": customer["id"],
+                "text": customer["first_name"]
+                + " "
+                + customer["last_name"]
+                + " ("
+                + customer["email"]
+                + ")",
+            }
+            for customer in people
+        ]
         return Response({"results": data_transform})
 
-    @detail_route(methods=["POST",], detail=True,)
+    @list_route(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+    )
+    @basic_exception_handler
+    def get_department_users(self, request, *args, **kwargs):
+        search_term = request.GET.get("term", "")
+        data = (
+            EmailUser.objects.filter(is_staff=True)
+            .filter(
+                Q(first_name__icontains=search_term)
+                | Q(last_name__icontains=search_term)
+            )
+            .values("email", "first_name", "last_name")[:10]
+        )
+        data_transform = [
+            {
+                "id": person["email"],
+                "text": person["first_name"] + " " + person["last_name"],
+            }
+            for person in data
+        ]
+        return Response({"results": data_transform})
+
+    @detail_route(
+        methods=[
+            "POST",
+        ],
+        detail=True,
+    )
     @basic_exception_handler
     def update_personal(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -139,7 +194,12 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(instance)
         return Response(serializer.data)
 
-    @detail_route(methods=["POST",], detail=True,)
+    @detail_route(
+        methods=[
+            "POST",
+        ],
+        detail=True,
+    )
     @basic_exception_handler
     def update_contact(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -149,7 +209,12 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(instance)
         return Response(serializer.data)
 
-    @detail_route(methods=["POST",], detail=True,)
+    @detail_route(
+        methods=[
+            "POST",
+        ],
+        detail=True,
+    )
     @basic_exception_handler
     def update_address(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -190,15 +255,18 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(instance)
         return Response(serializer.data)
 
-    @detail_route(methods=["POST",], detail=True,)
+    @detail_route(
+        methods=[
+            "POST",
+        ],
+        detail=True,
+    )
     @basic_exception_handler
     def update_system_settings(self, request, *args, **kwargs):
         instance = self.get_object()
         # serializer = UserSystemSettingsSerializer(data=request.data)
         # serializer.is_valid(raise_exception=True)
-        user_setting, created = UserSystemSettings.objects.get_or_create(
-            user=instance
-        )
+        user_setting, created = UserSystemSettings.objects.get_or_create(user=instance)
         serializer = UserSystemSettingsSerializer(user_setting, data=request.data)
         serializer.is_valid(raise_exception=True)
         # instance.residential_address = address
@@ -207,25 +275,35 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(instance)
         return Response(serializer.data)
 
-    @detail_route(methods=["POST",], detail=True,)
-    @basic_exception_handler
-    def upload_id(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.upload_identification(request)
-        with transaction.atomic():
-            instance.save()
-            instance.log_user_action(
-                EmailUserAction.ACTION_ID_UPDATE.format(
-                    "{} {} ({})".format(
-                        instance.first_name, instance.last_name, instance.email
-                    )
-                ),
-                request,
-            )
-        serializer = UserSerializer(instance, partial=True)
-        return Response(serializer.data)
+    # @detail_route(
+    #     methods=[
+    #         "POST",
+    #     ],
+    #     detail=True,
+    # )
+    # @basic_exception_handler
+    # def upload_id(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     instance.upload_identification(request)
+    #     with transaction.atomic():
+    #         instance.save()
+    #         instance.log_user_action(
+    #             EmailUserAction.ACTION_ID_UPDATE.format(
+    #                 "{} {} ({})".format(
+    #                     instance.first_name, instance.last_name, instance.email
+    #                 )
+    #             ),
+    #             request,
+    #         )
+    #     serializer = UserSerializer(instance, partial=True)
+    #     return Response(serializer.data)
 
-    @detail_route(methods=["GET",], detail=True,)
+    @detail_route(
+        methods=[
+            "GET",
+        ],
+        detail=True,
+    )
     @basic_exception_handler
     def pending_org_requests(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -236,23 +314,38 @@ class UserViewSet(viewsets.ModelViewSet):
         )
         return Response(serializer.data)
 
-    @detail_route(methods=["GET",], detail=True,)
-    @basic_exception_handler
-    def action_log(self, request, *args, **kwargs):
-        instance = self.get_object()
-        qs = instance.action_logs.all()
-        serializer = EmailUserActionSerializer(qs, many=True)
-        return Response(serializer.data)
+    # @detail_route(
+    #     methods=[
+    #         "GET",
+    #     ],
+    #     detail=True,
+    # )
+    # @basic_exception_handler
+    # def action_log(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     qs = instance.action_logs.all()
+    #     serializer = EmailUserActionSerializer(qs, many=True)
+    #     return Response(serializer.data)
 
-    @detail_route(methods=["GET",], detail=True,)
-    @basic_exception_handler
-    def comms_log(self, request, *args, **kwargs):
-        instance = self.get_object()
-        qs = instance.comms_logs.all()
-        serializer = EmailUserCommsSerializer(qs, many=True)
-        return Response(serializer.data)
+    # @detail_route(
+    #     methods=[
+    #         "GET",
+    #     ],
+    #     detail=True,
+    # )
+    # @basic_exception_handler
+    # def comms_log(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     qs = instance.comms_logs.all()
+    #     serializer = EmailUserCommsSerializer(qs, many=True)
+    #     return Response(serializer.data)
 
-    @detail_route(methods=["POST",], detail=True,)
+    @detail_route(
+        methods=[
+            "POST",
+        ],
+        detail=True,
+    )
     @renderer_classes((JSONRenderer,))
     @basic_exception_handler
     def add_comms_log(self, request, *args, **kwargs):
@@ -260,10 +353,10 @@ class UserViewSet(viewsets.ModelViewSet):
             instance = self.get_object()
             mutable = request.data._mutable
             request.data._mutable = True
-            request.data["emailuser"] = "{}".format(instance.id)
-            request.data["staff"] = "{}".format(request.user.id)
+            request.data["emailuser"] = f"{instance.id}"
+            request.data["staff"] = f"{request.user.id}"
             request.data._mutable = mutable
-            serializer = EmailUserLogEntrySerializer(data=request.data)
+            serializer = None  # EmailUserLogEntrySerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             comms = serializer.save()
             # Save the files
