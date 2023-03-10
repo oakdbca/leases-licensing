324892a4c2e3e8f02dcc56750849bf8bee703358
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-10 14:16:21 +0800

    Show system name in welcome message.

--- leaseslicensing/context_processors.py
+++ leaseslicensing/context_processors.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 
+from leaseslicensing.settings import template_group, template_title
+
 
 def leaseslicensing_url(request):
     return {
         "KMI_SERVER_URL": settings.KMI_SERVER_URL,
-        "template_group": settings.template_group,
-        "template_title": settings.template_title,
+        "template_group": template_group,
+        "template_title": template_title,
         "build_tag": settings.BUILD_TAG,
         "app_build_url": settings.DEV_APP_BUILD_URL,
     }
