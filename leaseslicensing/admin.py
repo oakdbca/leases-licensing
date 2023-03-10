fa0f5e421a485e3e6abfab7a06f73c28b0de1e7b
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-10 13:57:07 +0800

    Clean up.

--- leaseslicensing/admin.py
+++ leaseslicensing/admin.py
@@ -1,23 +1,11 @@
 from django.contrib.gis import admin
-from leaseslicensing.components.main import models
-
-from django.contrib.admin import AdminSite
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
-
-from django.db.models import Q
-
-# from ledger.accounts.models import EmailUser
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
-from django.http import HttpResponse
-
-from copy import deepcopy
 
 admin.site.index_template = "admin-index.html"
 admin.autodiscover()
 
 
-@admin.register(models.EmailUser)
+@admin.register(EmailUser)
 class EmailUserAdmin(admin.ModelAdmin):
     list_display = (
         "email",
@@ -29,10 +17,15 @@ class EmailUserAdmin(admin.ModelAdmin):
     ordering = ("email",)
     search_fields = ("id", "email", "first_name", "last_name")
 
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.opts.verbose_name_plural = "Email Users (Read-Only)"
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
     def has_change_permission(self, request, obj=None):
-        if obj is None:  # and obj.status > 1:
-            return True
-        return None
+        return False
 
     def has_delete_permission(self, request, obj=None):
-        return None
+        return False
