949d7d2a9f854f05014f1e807f3995055d7b127f
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-10 12:28:15 +0800

    Remove unused security group models (We should be able to just use system groups).

--- leaseslicensing/components/main/models.py
+++ leaseslicensing/components/main/models.py
@@ -1,64 +1,8 @@
-from __future__ import unicode_literals
 import os
 
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import pre_delete
 
-# from django.utils.encoding import python_2_unicode_compatible
-from django.core.exceptions import ValidationError
-
-# from ledger.accounts.models import EmailUser, Document, RevisionedMixin
-from ledger_api_client.ledger_models import (
-    EmailUserRO as EmailUser,
-    BaseAddress,
-)  # , RevisionedMixin
-
-# from django.contrib.postgres.fields.jsonb import JSONField
-from django.db.models import JSONField
 from leaseslicensing import settings
-from leaseslicensing.ledger_api_utils import retrieve_email_user
-
-
-## TODO: remove ledger models
-
-##@python_2_unicode_compatible
-# class Organisation(models.Model):
-#    """This model represents the details of a company or other organisation.
-#    Management of these objects will be delegated to 0+ EmailUsers.
-#    """
-#    name = models.CharField(max_length=128, unique=True)
-#    abn = models.CharField(max_length=50, null=True, blank=True, verbose_name='ABN')
-#    # TODO: business logic related to identification file upload/changes.
-#    identification = models.FileField(upload_to='%Y/%m/%d', null=True, blank=True)
-#    postal_address = models.ForeignKey('OrganisationAddress', related_name='org_postal_address', blank=True, null=True, on_delete=models.SET_NULL)
-#    billing_address = models.ForeignKey('OrganisationAddress', related_name='org_billing_address', blank=True, null=True, on_delete=models.SET_NULL)
-#    email = models.EmailField(blank=True, null=True,)
-#    trading_name = models.CharField(max_length=256, null=True, blank=True)
-#
-#    def upload_identification(self, request):
-#        with transaction.atomic():
-#            self.identification = request.data.dict()['identification']
-#            self.save()
-#
-#    def __str__(self):
-#        return self.name
-#
-#    class Meta:
-#        #abstract = True
-#        managed = False
-#
-# class OrganisationAddress(BaseAddress):
-#    organisation = models.ForeignKey(Organisation, null=True,blank=True, related_name='adresses', on_delete=models.CASCADE)
-#    class Meta:
-#        verbose_name_plural = 'organisation addresses'
-#        unique_together = ('organisation','hash')
-#
-#    class Meta:
-#        #abstract = True
-#        managed = False
-
-#####
 
 
 class MapLayer(models.Model):
@@ -76,7 +20,7 @@ class MapLayer(models.Model):
         verbose_name = "map layer"
 
     def __str__(self):
-        return "{0}, {1}".format(self.display_name, self.layer_name)
+        return f"{self.display_name}, {self.layer_name}"
 
     @property
     def column_names(self):
@@ -103,7 +47,7 @@ class MapColumn(models.Model):
         verbose_name = "map column"
 
     def __str__(self):
-        return "{0}, {1}".format(self.map_layer, self.name)
+        return f"{self.map_layer}, {self.name}"
 
 
 class RevisionedMixin(models.Model):
@@ -115,34 +59,31 @@ class RevisionedMixin(models.Model):
         from reversion import revisions
 
         if kwargs.pop("no_revision", False):
-            super(RevisionedMixin, self).save(**kwargs)
+            super().save(**kwargs)
         else:
             with revisions.create_revision():
                 if "version_user" in kwargs:
                     revisions.set_user(kwargs.pop("version_user", None))
                 if "version_comment" in kwargs:
                     revisions.set_comment(kwargs.pop("version_comment", ""))
-                super(RevisionedMixin, self).save(**kwargs)
+                super().save(**kwargs)
 
     @property
     def created_date(self):
         from reversion.models import Version
 
-        # return revisions.get_for_object(self).last().revision.date_created
         return Version.objects.get_for_object(self).last().revision.date_created
 
     @property
     def modified_date(self):
         from reversion.models import Version
 
-        # return revisions.get_for_object(self).first().revision.date_created
         return Version.objects.get_for_object(self).first().revision.date_created
 
     class Meta:
         abstract = True
 
 
-# @python_2_unicode_compatible
 class RequiredDocument(models.Model):
     question = models.TextField(blank=False)
 
@@ -153,7 +94,6 @@ class RequiredDocument(models.Model):
         return self.question
 
 
-# @python_2_unicode_compatible
 class ApplicationType(models.Model):
     name = models.CharField(
         max_length=64, unique=True, choices=settings.APPLICATION_TYPES
@@ -176,7 +116,7 @@ class ApplicationType(models.Model):
     def get_application_type_by_name(name):
         try:
             return ApplicationType.objects.get(name=name)
-        except:
+        except ApplicationType.DoesNotExist:
             return None
 
     @property
@@ -221,7 +161,7 @@ class OracleCode(models.Model):
         app_label = "leaseslicensing"
 
     def __str__(self):
-        return "{} - {}".format(self.code_type, self.code)
+        return f"{self.code_type} - {self.code}"
 
 
 # @python_2_unicode_compatible
@@ -331,7 +271,8 @@ class Document(models.Model):
     def path(self):
         # return self.file.path
         # return self._file.path
-        # comment above line to fix the error "The '_file' attribute has no file associated with it." when adding comms log entry.
+        # comment above line to fix the error "The '_file' attribute has no
+        # file associated with it." when adding comms log entry.
         if self._file:
             return self._file.path
         else:
@@ -403,10 +344,9 @@ class SystemMaintenance(models.Model):
 
 
 class UserSystemSettings(models.Model):
-    one_row_per_park = models.BooleanField(
-        default=False
-    )  # Setting for user if they want to see Payment (Park Entry Fees Dashboard) by one row per park or one row per booking
-    # user = models.OneToOneField(EmailUser, related_name='system_settings', on_delete=models.CASCADE)
+    one_row_per_park = models.BooleanField(default=False)
+    # Setting for user if they want to see Payment (Park Entry Fees Dashboard)
+    # by one row per park or one row per booking
     user = models.IntegerField(unique=True)  # EmailUserRO
     event_training_completed = models.BooleanField(default=False)
     event_training_date = models.DateField(blank=True, null=True)
@@ -415,80 +355,19 @@ class UserSystemSettings(models.Model):
         app_label = "leaseslicensing"
         verbose_name_plural = "User System Settings"
 
-# Not ready for use
-class SecurityGroup(models.Model):
-
-    #name = models.CharField(max_length=150, unique=True)
-    name = models.CharField(
-        max_length=200,
-        choices=settings.GROUP_NAME_CHOICES)
-    #group_email = models.CharField(max_length=255, blank=True)
-
-    class Meta:
-        app_label = 'leaseslicensing'
-
-    def __str__(self):
-        return self.name
-
-    def get_members(self):
-        return [retrieve_email_user(member.emailuser) for member in self.securitygroupmembership_set.all()]
-
-    def add_member(self, user_id):
-        SecurityGroupMembership.objects.create(group=self,emailuser=user_id)
-
-
-# Not ready for use
-class SecurityGroupMembership(models.Model):
-    group = models.ForeignKey(SecurityGroup, on_delete=models.PROTECT)
-    #emailuser = models.ForeignKey(EmailUser, on_delete=models.PROTECT, blank=True, null=True, db_constraint=False)
-    emailuser = models.IntegerField(null=True)  # EmailUserRO
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        app_label = 'leaseslicensing'
-
-    def __str__(self):
-        return str(self.group)
-
 
 class TemporaryDocumentCollection(models.Model):
-
     class Meta:
-        app_label = 'leaseslicensing'
+        app_label = "leaseslicensing"
 
 
 class TemporaryDocument(Document):
     temp_document_collection = models.ForeignKey(
         TemporaryDocumentCollection,
-        related_name='documents',
+        related_name="documents",
         on_delete=models.CASCADE,
     )
     _file = models.FileField(max_length=255)
 
     class Meta:
-        app_label = 'leaseslicensing'
-
-
-# import reversion
-# reversion.register(Region, follow=['districts'])
-# reversion.register(District, follow=['parks'])
-##reversion.register(AccessType)
-# reversion.register(AccessType, follow=['park_set', 'proposalparkaccess_set', 'vehicles'])
-# reversion.register(ActivityType)
-# reversion.register(ActivityCategory, follow=['activities'])
-##reversion.register(Activity, follow=['park_set', 'zone_set', 'trail_set', 'requireddocument_set'])
-# reversion.register(Activity, follow=['park_set', 'zone_set', 'trail_set', 'requireddocument_set', 'proposalparkactivity_set','proposalparkzoneactivity_set', 'proposaltrailsectionactivity_set'])
-##reversion.register(Park, follow=['zones', 'requireddocument_set', 'proposals', 'park_entries', 'bookings'])
-# reversion.register(Park, follow=['zones', 'requireddocument_set', 'proposals'])
-# reversion.register(Zone, follow=['proposal_zones'])
-# reversion.register(Trail, follow=['sections', 'proposals'])
-# reversion.register(Section, follow=['proposal_trails'])
-# reversion.register(RequiredDocument)
-# reversion.register(ApplicationType, follow=['tenure_app_types', 'helppage_set'])
-# reversion.register(ActivityMatrix)
-# reversion.register(Tenure)
-# reversion.register(Question)
-# reversion.register(UserAction)
-# reversion.register(CommunicationsLogEntry)
-# reversion.register(Document)
-# reversion.register(SystemMaintenance)
+        app_label = "leaseslicensing"
