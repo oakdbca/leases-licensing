a5efb495e8b95649cb013cc20558facf62b93a13
Jason Moore <jmoore@austwa.com>
2023-03-14 11:12:43 +0800

    Update pre_startup.sh

--- pre_startup.sh
+++ pre_startup.sh
@@ -6,7 +6,7 @@ cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 # elevation permissions required
 sudo /startup.sh
-pipenv run gunicorn leaseslicensing.wsgi --bind :8080 --config /app/gunicorn.ini
+poetry run gunicorn leaseslicensing.wsgi --bind :8080 --config /app/gunicorn.ini
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start gunicorn: $status"
