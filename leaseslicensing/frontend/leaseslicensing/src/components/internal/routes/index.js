1edcb0ff2f467bfee52e43e48b81701b62782cf2
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-24 10:18:12 +0800

    Clean up.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/routes/index.js
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/routes/index.js
@@ -1,34 +1,33 @@
 
-import { RouterView } from 'vue-router'
-import InternalDashboard from '@/components/internal/dashboard.vue'
-import OrgAccessTable from '@/components/internal/organisations/dashboard.vue'
-import OrgAccess from '@/components/internal/organisations/access.vue'
-import Organisation from '@/components/internal/organisations/manage.vue'
-import Proposal from '@/components/internal/proposals/proposal.vue'
-import ApprovalDash from '@/components/internal/approvals/dashboard.vue'
-import ComplianceDash from '@/components/internal/compliances/dashboard.vue'
-import InvoicesDash from '@/components/internal/invoices/dashboard.vue'
-import Search from '@/components/internal/search/dashboard.vue'
-import PersonDetail from '@/components/internal/person/person_detail.vue'
-import Compliance from '../compliances/access.vue'
-import Reports from '@/components/reports/reports.vue'
-import Approval from '@/components/internal/approvals/approval.vue'
-import CompetitiveProcess from '@/components/internal/competitive_process/competitive_process.vue'
+import { RouterView } from 'vue-router';
+import InternalDashboard from '@/components/internal/dashboard.vue';
+import OrgAccessTable from '@/components/internal/organisations/dashboard.vue';
+import OrgAccess from '@/components/internal/organisations/access.vue';
+import Organisation from '@/components/internal/organisations/manage.vue';
+import Proposal from '@/components/internal/proposals/proposal.vue';
+import ApprovalDash from '@/components/internal/approvals/dashboard.vue';
+import ComplianceDash from '@/components/internal/compliances/dashboard.vue';
+import InvoicesDash from '@/components/internal/invoices/dashboard.vue';
+import Search from '@/components/internal/search/dashboard.vue';
+import PersonDetail from '@/components/internal/person/person_detail.vue';
+import Compliance from '../compliances/access.vue';
+import Reports from '@/components/reports/reports.vue';
+import Approval from '@/components/internal/approvals/approval.vue';
+import CompetitiveProcess from '@/components/internal/competitive_process/competitive_process.vue';
 
-export default
-{
+export default {
     path: '/internal',
     component: RouterView,
     children: [
         {
             path: '/internal',
             component: InternalDashboard,
-            name: "internal-dashboard"
+            name: 'internal-dashboard'
         },
         {
             path: 'approvals',
             component: ApprovalDash,
-            name:"internal-approvals-dash"
+            name: 'internal-approvals-dash'
         },
         {
             path: 'approval/:approval_id',
@@ -38,7 +37,7 @@ export default
         {
             path: 'compliances',
             component: ComplianceDash,
-            name: "internal-compliances-dash"
+            name: 'internal-compliances-dash'
         },
         {
             path: 'compliance/:compliance_id',
@@ -48,12 +47,12 @@ export default
         {
             path: 'invoices',
             component: InvoicesDash,
-            name: "internal-invoices-dash"
+            name: 'internal-invoices-dash'
         },
         {
             path: 'search',
             component: Search,
-            name:"internal-search"
+            name: 'internal-search'
         },
         {
             path: 'person/details/:id',
@@ -61,9 +60,9 @@ export default
             name: 'internal-person-detail'
         },
         {
-            path:'reports',
-            name:'reports',
-            component:Reports
+            path: 'reports',
+            name: 'reports',
+            component: Reports
         },
         {
             path: 'organisations',
@@ -72,22 +71,17 @@ export default
                 {
                     path: 'access',
                     component: OrgAccessTable,
-                    name:"org-access-dash"
-                },
-                {
-                    path: 'access',
-                    component: OrgAccessTable,
-                    name:"org-access-dash"
+                    name: 'org-access-dash'
                 },
                 {
                     path: 'access/:access_id',
                     component: OrgAccess,
-                    name:"org-access"
+                    name: 'org-access'
                 },
                 {
                     path: ':org_id',
                     component: Organisation,
-                    name:"internal-org-detail"
+                    name: 'internal-org-detail'
                 },
 
             ]
@@ -103,4 +97,4 @@ export default
             name: 'internal-competitive-process',
         },
     ]
-}
+};
