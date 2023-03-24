74195265806717afd57507c593b007d9f40ba880
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-24 10:13:51 +0800

    Configuration of OrganisationAdmin.

--- leaseslicensing/components/organisations/admin.py
+++ leaseslicensing/components/organisations/admin.py
@@ -11,18 +11,57 @@ class OrganisationAdmin(admin.ModelAdmin):
     list_display = [
         "organisation",
         "organisation_name",
+        "organisation_abn",
         "admin_pin_one",
         "admin_pin_two",
         "user_pin_one",
         "user_pin_two",
     ]
     search_fields = (
-        "organisation__name",
+        "organisation_name",
+        "organisation_abn",
         "admin_pin_one",
         "admin_pin_two",
         "user_pin_one",
         "user_pin_two",
     )
+    fields = [
+        "organisation",
+        "organisation_name",
+        "organisation_abn",
+        "delegates",
+        "admin_pin_one",
+        "admin_pin_two",
+        "user_pin_one",
+        "user_pin_two",
+        "bpay_allowed",
+        "monthly_invoicing_allowed",
+        "monthly_invoicing_period",
+        "monthly_payment_due_period",
+        "apply_application_discount",
+        "apply_licence_discount",
+        "licence_discount",
+        "event_training_completed",
+        "event_training_date",
+        "charge_once_per_year",
+        "last_event_application_fee_date",
+        "max_num_months_ahead",
+    ]
+    readonly_fields = ["organisation_name", "organisation_abn"]
+
+    def organisation_name(self, obj):
+        if obj.organisation:
+            return obj.organisation["organisation_name"]
+        return "No Organisation Assigned"
+
+    organisation_name.short_description = "Ledger Organisation Name"
+
+    def organisation_abn(self, obj):
+        if obj.organisation:
+            return obj.organisation["organisation_abn"]
+        return "No Organisation Assigned"
+
+    organisation_abn.short_description = "Ledger Organisation ABN"
 
 
 @admin.register(models.OrganisationRequest)
