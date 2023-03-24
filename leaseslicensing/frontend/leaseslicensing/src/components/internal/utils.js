3771defd44886550b6b82c5b5779a025c144e043
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-24 10:17:27 +0800

    Updated method to use fetch.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/utils.js
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/utils.js
@@ -1,77 +1,101 @@
-import api from './api'
-import {helpers} from '@/utils/hooks' 
+import { api_endpoints, helpers } from '@/utils/hooks'
 
 export default {
-    fetchProposal: function(id){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(helpers.add_endpoint_json(api.proposals,id)).then((response) => {
+    fetchProposal: function (id) {
+        return new Promise((resolve, reject) => {
+            this.$http.get(helpers.add_endpoint_json(api_endpoints.proposals, id)).then((response) => {
                 resolve(response.body);
             },
-            (error) => {
-                reject(error);
-            });
+                (error) => {
+                    reject(error);
+                });
         });
     },
-    fetchOrganisations: function(id){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(api.organisations).then((response) => {
+    fetchOrganisations: function (id) {
+        return new Promise((resolve, reject) => {
+            this.$http.get(api_endpoints.organisations).then((response) => {
                 resolve(response.body);
             },
-            (error) => {
-                reject(error);
-            });
+                (error) => {
+                    reject(error);
+                });
         });
     },
-    fetchCountries: function (){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(api.countries).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
-        });
-
-    },
-    fetchOrganisation: function(id){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(helpers.add_endpoint_json(api.organisations,id)).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
+    fetchCountries: function () {
+        return new Promise((resolve, reject) => {
+            fetch(api_endpoints.countries)
+                .then(async (response) => {
+                    const data = await response.json()
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText
+                        console.log(error)
+                        return Promise.reject(error)
+                    }
+                    resolve(data)
+                    console.log('countries: ', data)
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error)
+                })
         });
     },
-    fetchUser: function(id){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(helpers.add_endpoint_json(api.users,id)).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
+    fetchOrganisation: function (id) {
+        return new Promise((resolve, reject) => {
+            fetch(api_endpoints.organisations_viewset + id + '/')
+                .then(async (response) => {
+                    const data = await response.json()
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText
+                        console.log(error)
+                        return Promise.reject(error)
+                    }
+                    console.log('organisation: ', data)
+                    resolve(data)
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error)
+                })
         });
     },
-    fetchOrgRequestPending:function (id){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(helpers.add_endpoint_json(api.users,id + '/pending_org_requests')).then((response) => {
+    fetchUser: function (id) {
+        return new Promise((resolve, reject) => {
+            this.$http.get(helpers.add_endpoint_json(api_endpoints.users, id)).then((response) => {
                 resolve(response.body);
             },
-            (error) => {
-                reject(error);
-            });
+                (error) => {
+                    reject(error);
+                });
         });
     },
-    fetchProfile: function (){
-        return new Promise ((resolve,reject) => {
-            this.$http.get(api.profile).then((response) => {
+    fetchOrgRequestPending: function (id) {
+        return new Promise((resolve, reject) => {
+            this.$http.get(helpers.add_endpoint_json(api_endpoints.users, id + '/pending_org_requests')).then((response) => {
                 resolve(response.body);
             },
-            (error) => {
-                reject(error);
-            });
+                (error) => {
+                    reject(error);
+                });
         });
-
+    },
+    fetchProfile: function () {
+        return new Promise((resolve, reject) => {
+            fetch(api_endpoints.profile)
+                .then(async (response) => {
+                    const data = await response.json()
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText
+                        console.log(error)
+                        return Promise.reject(error)
+                    }
+                    resolve(data)
+                    console.log('profile: ', data)
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error)
+                })
+        });
     },
 }
