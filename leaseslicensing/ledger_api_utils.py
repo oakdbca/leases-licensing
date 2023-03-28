d465c056f040f259cc428b100c8d54fc1bff6806
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-28 15:10:48 +0800

    Add basic caching to retrieve_email_user. Will find a way to change this to a per request cache to avoid issues in future.

--- leaseslicensing/ledger_api_utils.py
+++ leaseslicensing/ledger_api_utils.py
@@ -18,8 +18,8 @@ def retrieve_email_user(email_user_id):
     cache_key = settings.CACHE_KEY_LEDGER_EMAIL_USER.format(email_user_id)
     email_user = cache.get(cache_key)
     if email_user is None:
+        logger.debug(f"{cache_key}:{email_user}")
         email_user = EmailUser.objects.get(id=email_user_id)
     # Todo: A per request cache would be best here
     cache.set(cache_key, email_user, settings.CACHE_TIMEOUT_1_MINUTE)
-    logger.debug(f"{cache_key}:{email_user}")
     return email_user
