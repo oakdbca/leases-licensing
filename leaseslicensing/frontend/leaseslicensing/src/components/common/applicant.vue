78a26aed0e026690e8a6954a062135a3f8143779
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-29 16:35:11 +0800

    Added update buttons from design image (not yet implemented). Modified form look to suit design. Added collapseFormSections and readonly props.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/applicant.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/applicant.vue
@@ -2,40 +2,42 @@
     <div class="container m-0 p-0">
         <div class="row">
             <div class="col-md-12" id="ledgeraccount">
-                <FormSection v-if="email_user.personal_details" :formCollapse="true" label="Personal Details"
-                    index="personal-details">
+                <FormSection v-if="email_user.personal_details" :formCollapse="collapseFormSections"
+                    label="Personal Details" index="personal-details">
                     <form>
                         <div class="row mb-2">
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <label for="firstName" class="form-label">First Name</label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-4">
                                 <input type="text" class="form-control" id="firstName" name="firstName"
                                     v-model="email_user.first_name" :readonly="readonly" />
                             </div>
                         </div>
                         <div class="row mb-2">
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <label for="lastName" class="form-label">Last Name</label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-4">
                                 <input type="text" class="form-control" id="lastName" name="lastName"
                                     v-model="email_user.last_name" :readonly="readonly" />
                             </div>
                         </div>
+                        <button v-if="!readonly" @click.prevent="updatePersonalDetails"
+                            class="btn btn-primary float-end">Update</button>
                     </form>
                 </FormSection>
-                <FormSection v-if="email_user.address_details" :formCollapse="true" label="Address Details"
+                <FormSection v-if="email_user.address_details" :formCollapse="collapseFormSections" label="Address Details"
                     index="address-details">
-                    <form v-if="email_user.residential_address" class="form-horizontal">
+                    <form v-if="email_user.residential_address" class="form-horizontal mb-2">
                         <fieldset>
                             <legend>Residential Address</legend>
                             <div class="address-box">
                                 <div class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialAddressLine1" class="form-label">Line 1</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <input type="text" class="form-control" id="residentialAddressLine1"
                                             name="residentialAddressLine1" v-model="
                                                 email_user.residential_address
@@ -44,10 +46,10 @@
                                     </div>
                                 </div>
                                 <div v-if="email_user.residential_address.line2" class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialAddressLine2" class="form-label">Line 2</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <input type="text" class="form-control" id="residentialAddressLine2"
                                             name="residentialAddressLine2" v-model="
                                                 email_user.residential_address
@@ -56,10 +58,10 @@
                                     </div>
                                 </div>
                                 <div class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialLocality" class="form-label">Town/Suburb</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <input type="text" class="form-control" id="residentialLocality"
                                             name="residentialLocality" v-model="
                                                 email_user.residential_address
@@ -68,10 +70,10 @@
                                     </div>
                                 </div>
                                 <div class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialState" class="form-label">State</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <input type="text" class="form-control" id="residentialState"
                                             name="residentialState" v-model="
                                                 email_user.residential_address
@@ -80,10 +82,10 @@
                                     </div>
                                 </div>
                                 <div class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialPostcode" class="form-label">Postcode</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <input type="text" class="form-control" id="residentialPostcode"
                                             name="residentialPostcode" v-model="
                                                 email_user.residential_address
@@ -92,10 +94,10 @@
                                     </div>
                                 </div>
                                 <div class="row mb-2">
-                                    <div class="col-md-3">
+                                    <div class="col-md-2">
                                         <label for="residentialPostcode" class="form-label">Country</label>
                                     </div>
-                                    <div class="col-md-9">
+                                    <div class="col-md-4">
                                         <select class="form-control" id="country" name="Country" v-model="
                                             email_user.residential_address
                                                 .country
@@ -109,7 +111,7 @@
                             </div>
                         </fieldset>
                     </form>
-                    <form v-if="email_user.postal_address" class="form-horizontal">
+                    <form v-if="email_user.postal_address" class="form-horizontal mb-2">
                         <fieldset>
                             <legend>Postal Address</legend>
                             <div class="form-group">
@@ -170,37 +172,41 @@
                             </div>
                         </fieldset>
                     </form>
+                    <button v-if="!readonly" @click.prevent="updateAddressDetails"
+                        class="btn btn-primary float-end">Update</button>
                 </FormSection>
-                <FormSection :formCollapse="true" label="Contact Details" index="contact-details">
+                <FormSection :formCollapse="collapseFormSections" label="Contact Details" index="contact-details">
                     <form>
                         <div v-if="email_user.phone_number" class="row mb-2">
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <label for="phone" class="form-label">Phone</label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-4">
                                 <input type="text" class="form-control" id="phone" name="phone"
                                     v-model="email_user.phone_number" :readonly="readonly" />
                             </div>
                         </div>
                         <div v-if="email_user.mobile_number" class="row mb-2">
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <label for="mobile" class="form-label">Mobile</label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-4">
                                 <input type="text" class="form-control" id="mobile" name="mobile"
                                     v-model="email_user.mobile_number" :readonly="readonly" />
                             </div>
                         </div>
                         <div v-if="email_user.email" class="row mb-2">
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <label for="email" class="form-label">Email</label>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-4">
                                 <input type="text" class="form-control" id="email" name="email" v-model="email_user.email"
                                     :readonly="readonly" />
                             </div>
                         </div>
                     </form>
+                    <button v-if="!readonly" @click.prevent="updateContactDetails"
+                        class="btn btn-primary float-end">Update</button>
                 </FormSection>
             </div>
         </div>
@@ -224,13 +230,20 @@ export default {
         proposalId: {
             type: Number,
         },
+        collapseFormSections: {
+            type: Boolean,
+            default: true,
+        },
+        readonly: {
+            type: Boolean,
+            default: true,
+        },
     },
     data: function () {
         let vm = this
         return {
             electoralRollSectionIndex: 'electoral_roll_' + vm._uid,
             silentElector: null,
-            readonly: true,
             values: null,
             countries: [],
             showAddressError: false,
@@ -261,6 +274,15 @@ export default {
         },
     },
     methods: {
+        updatePersonalDetails: function () {
+            alert('To be implemented');
+        },
+        updateAddressDetails: function () {
+            alert('To be implemented');
+        },
+        updateContactDetails: function () {
+            alert('To be implemented');
+        },
         fetchCountries: function (id) {
             let vm = this
             fetch(api_endpoints.countries)
