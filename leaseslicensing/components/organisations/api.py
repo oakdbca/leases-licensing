1bc6b6f20f04c29346a62488ea15ab1f35222cd5
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-03-30 14:02:26 +0800

    Remove dead code.

--- leaseslicensing/components/organisations/api.py
+++ leaseslicensing/components/organisations/api.py
@@ -348,23 +348,6 @@ class OrganisationViewSet(UserActionLoggingViewset):
         serializer = OrganisationActionSerializer(qs, many=True)
         return Response(serializer.data)
 
-    #    @detail_route(methods=['GET',])
-    #    def applications(self, request, *args, **kwargs):
-    #        try:
-    #            instance = self.get_object()
-    #            qs = instance.org_applications.all()
-    #            serializer = BaseApplicationSerializer(qs,many=True)
-    #            return Response(serializer.data)
-    #        except serializers.ValidationError:
-    #            print(traceback.print_exc())
-    #            raise
-    #        except ValidationError as e:
-    #            print(traceback.print_exc())
-    #            raise serializers.ValidationError(repr(e.error_dict))
-    #        except Exception as e:
-    #            print(traceback.print_exc())
-    #            raise serializers.ValidationError(str(e))
-
     @detail_route(
         methods=[
             "GET",
