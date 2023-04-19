e448df5407f80e767fe74810eefcf8974827b6f9
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-04-19 16:43:08 +0800

    Moved tab nav css to base.css so it is glocally available.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/dashboard.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/dashboard.vue
@@ -3,82 +3,48 @@
         <div v-if="is_debug">src/components/internal/dashboard.vue</div>
         <ul class="nav nav-pills" id="pills-tab" role="tablist">
             <li class="nav-item">
-                <a
-                    class="nav-link"
-                    id="pills-applications-tab"
-                    data-bs-toggle="pill"
-                    href="#pills-applications"
-                    role="tab"
-                    aria-controls="pills-applications"
-                    aria-selected="true"
-                    @click="tabClicked('applications')"
-                >Applications</a>
+                <a class="nav-link" id="pills-applications-tab" data-bs-toggle="pill" href="#pills-applications" role="tab"
+                    aria-controls="pills-applications" aria-selected="true"
+                    @click="tabClicked('applications')">Applications</a>
             </li>
             <li class="nav-item">
-                <a
-                    class="nav-link"
-                    id="pills-competitive-processes-tab"
-                    data-bs-toggle="pill"
-                    href="#pills-competitive-processes"
-                    role="tab"
-                    aria-controls="pills-competitive-processes"
-                    aria-selected="false"
-                    @click="tabClicked('competitive-processes')"
-                >Competitive Processes</a>
+                <a class="nav-link" id="pills-competitive-processes-tab" data-bs-toggle="pill"
+                    href="#pills-competitive-processes" role="tab" aria-controls="pills-competitive-processes"
+                    aria-selected="false" @click="tabClicked('competitive-processes')">Competitive Processes</a>
             </li>
             <li class="nav-item">
-                <a
-                    class="nav-link"
-                    id="pills-map-tab"
-                    data-bs-toggle="pill"
-                    href="#pills-map"
-                    role="tab"
-                    aria-controls="pills-map"
-                    aria-selected="false"
-                    @click="mapTabClicked"
-                >Map</a>
+                <a class="nav-link" id="pills-map-tab" data-bs-toggle="pill" href="#pills-map" role="tab"
+                    aria-controls="pills-map" aria-selected="false" @click="mapTabClicked">Map</a>
             </li>
         </ul>
 
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane active" id="pills-applications" role="tabpanel" aria-labelledby="pills-applications-tab">
                 <FormSection :formCollapse="false" label="Applications" Index="applications">
-                    <ApplicationsTable
-                        ref="applications_table"
-                        level="internal"
+                    <ApplicationsTable ref="applications_table" level="internal"
                         filterApplicationType_cache_name="filterApplicationTypeForApplicationTabley"
                         filterApplicationStatus_cache_name="filterApplicationStatusForApplicationTable"
                         filterApplicationLodgedFrom_cache_name="filterApplicationLodgedFromForApplicationTable"
-                        filterApplicationLodgedTo_cache_name="filterApplicationLodgedToForApplicationTable"
-                    />
+                        filterApplicationLodgedTo_cache_name="filterApplicationLodgedToForApplicationTable" />
                 </FormSection>
                 <FormSection :formCollapse="false" label="Applications referred to me" Index="leases_and_licences">
-                    <ApplicationsReferredToMeTable
-                        ref="applications_referred_to_me_table"
-                        v-if="accessing_user"
-                        level="internal"
-                        :email_user_id_assigned="accessing_user.id"
+                    <ApplicationsReferredToMeTable ref="applications_referred_to_me_table" v-if="accessing_user"
+                        level="internal" :email_user_id_assigned="accessing_user.id"
                         filterApplicationType_cache_name="filterApplicationTypeForApplicationReferredToMeTable"
                         filterApplicationStatus_cache_name="filterApplicationStatusForApplicationReferredToMeTable"
                         filterApplicationLodgedFrom_cache_name="filterApplicationLodgedFromForApplicationReferredToMeTable"
-                        filterApplicationLodgedTo_cache_name="filterApplicationLodgedToForApplicationReferredToMeTable"
-                    />
+                        filterApplicationLodgedTo_cache_name="filterApplicationLodgedToForApplicationReferredToMeTable" />
                 </FormSection>
             </div>
-            <div class="tab-pane" id="pills-competitive-processes" role="tabpanel" aria-labelledby="pills-competitive-processes-tab">
+            <div class="tab-pane" id="pills-competitive-processes" role="tabpanel"
+                aria-labelledby="pills-competitive-processes-tab">
                 <FormSection :formCollapse="false" label="Competitive Processes" Index="competitive_processes">
-                    <CompetitiveProcessesTable
-                        ref="competitive_processes_table"
-                        level="internal"
-                    />
+                    <CompetitiveProcessesTable ref="competitive_processes_table" level="internal" />
                 </FormSection>
             </div>
             <div class="tab-pane" id="pills-map" role="tabpanel" aria-labelledby="pills-map-tab">
                 <FormSection :formCollapse="false" label="Map" Index="map">
-                    <MapComponent
-                        ref="component_map_with_filters"
-                        level="internal"
-                    />
+                    <MapComponent ref="component_map_with_filters" level="internal" />
                 </FormSection>
             </div>
         </div>
@@ -112,7 +78,7 @@ export default {
             system_name: api_endpoints.system_name,
         }
     },
-    components:{
+    components: {
         FormSection,
         ApplicationsTable,
         ApplicationsReferredToMeTable,
@@ -127,32 +93,32 @@ export default {
 
     },
     computed: {
-        is_debug: function(){
+        is_debug: function () {
             return this.$route.query.hasOwnProperty('debug') && this.$route.query.debug == 'true' ? true : false
         },
-        is_external: function() {
+        is_external: function () {
             return this.level == 'external'
         },
-        is_internal: function() {
+        is_internal: function () {
             return this.level == 'internal'
         },
     },
     methods: {
-        tabClicked: function(param){
-            if (param == 'applications'){
+        tabClicked: function (param) {
+            if (param == 'applications') {
                 this.$refs.applications_table.adjust_table_width()
                 this.$refs.applications_referred_to_me_table.adjust_table_width()
-            } else if (param === 'competitive-processes'){
+            } else if (param === 'competitive-processes') {
                 this.$refs.competitive_processes_table.adjust_table_width()
             }
         },
-        mapTabClicked: function(){
+        mapTabClicked: function () {
             this.$refs.component_map_with_filters.forceToRefreshMap()
         },
-        set_active_tab: function(tab_href_name){
+        set_active_tab: function (tab_href_name) {
             let elem = $('#pills-tab a[href="#' + tab_href_name + '"]')
             let tab = bootstrap.Tab.getInstance(elem)
-            if(!tab)
+            if (!tab)
                 tab = new bootstrap.Tab(elem)
             tab.show()
         },
@@ -174,62 +140,35 @@ export default {
         const res = await fetch('/api/profile');
         const resData = await res.json();
         this.accessing_user = resData
-        this.$nextTick(function(){
+        this.$nextTick(function () {
             //vm.addEventListener()
             chevron_toggle.init();
             this.set_active_tab('pills-applications')
         })
     },
-    created: function() {
+    created: function () {
 
     },
 }
 </script>
 
 <style lang="css" scoped>
-    .section{
-        text-transform: capitalize;
-    }
-    .list-group{
-        margin-bottom: 0;
-    }
-    .fixed-top{
-        position: fixed;
-        top:56px;
-    }
+.section {
+    text-transform: capitalize;
+}
 
-    .nav-item {
-        margin-bottom: 2px;
-    }
+.list-group {
+    margin-bottom: 0;
+}
 
-    .nav-item>li>a {
-        background-color: yellow !important;
-        color: #fff;
-    }
+.fixed-top {
+    position: fixed;
+    top: 56px;
+}
 
-    .nav-item>li.active>a, .nav-item>li.active>a:hover, .nav-item>li.active>a:focus {
-      color: white;
-      background-color: blue;
-      border: 1px solid #888888;
-    }
-
-	.admin > div {
-	  display: inline-block;
-	  vertical-align: top;
-	  margin-right: 1em;
-	}
-    .nav-pills .nav-link {
-        border-bottom-left-radius: 0;
-        border-bottom-right-radius: 0;
-        border-top-left-radius: 0.5em;
-        border-top-right-radius: 0.5em;
-        margin-right: 0.25em;
-    }
-    .nav-pills .nav-link {
-        background: lightgray;
-    }
-    .nav-pills .nav-link.active {
-        background: gray;
-    }
+.admin>div {
+    display: inline-block;
+    vertical-align: top;
+    margin-right: 1em;
+}
 </style>
-
