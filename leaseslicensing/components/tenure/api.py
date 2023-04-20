fe25e4feac8eec6ca9e581b58b0a8748d49d6b61
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-04-20 19:54:48 +0800

    Optimised viewset with new NoPaginationMixin.

--- leaseslicensing/components/tenure/api.py
+++ leaseslicensing/components/tenure/api.py
@@ -2,7 +2,7 @@ import logging
 
 from rest_framework import viewsets
 
-from leaseslicensing.components.main.api import KeyValueListMixin
+from leaseslicensing.components.main.api import KeyValueListMixin, NoPaginationListMixin
 from leaseslicensing.components.tenure.models import LGA, Category, District, Region
 from leaseslicensing.components.tenure.serializers import (
     CategorySerializer,
@@ -15,42 +15,33 @@ from leaseslicensing.components.tenure.serializers import (
 logger = logging.getLogger(__name__)
 
 
-class RegionViewSet(viewsets.ModelViewSet, KeyValueListMixin):
+class RegionViewSet(viewsets.ModelViewSet, KeyValueListMixin, NoPaginationListMixin):
     model = Region
     serializer_class = RegionSerializer
     key_value_display_field = "name"
+    key_value_serializer_class = RegionSerializer
+    queryset = Region.objects.all()
 
-    def get_queryset(self):
-        return Region.objects.all()
 
-
-class DistrictViewSet(viewsets.ModelViewSet, KeyValueListMixin):
+class DistrictViewSet(viewsets.ModelViewSet, KeyValueListMixin, NoPaginationListMixin):
     model = District
     serializer_class = DistrictSerializer
+    key_value_serializer_class = DistrictKeyValueSerializer
     key_value_display_field = "name"
-
-    def get_queryset(self):
-        return District.objects.all()
-
-    def get_serializer_class(self):
-        if "key_value_list" == self.action:
-            return DistrictKeyValueSerializer
-        return super().get_serializer_class()
+    queryset = District.objects.all()
 
 
-class LGAViewSet(viewsets.ModelViewSet, KeyValueListMixin):
+class LGAViewSet(viewsets.ModelViewSet, KeyValueListMixin, NoPaginationListMixin):
     model = LGA
     serializer_class = LGASerializer
     key_value_display_field = "name"
+    key_value_serializer_class = LGASerializer
+    queryset = LGA.objects.all()
 
-    def get_queryset(self):
-        return LGA.objects.all()
 
-
-class CategoryViewSet(viewsets.ModelViewSet, KeyValueListMixin):
+class CategoryViewSet(viewsets.ModelViewSet, KeyValueListMixin, NoPaginationListMixin):
     model = Category
     serializer_class = CategorySerializer
     key_value_display_field = "name"
-
-    def get_queryset(self):
-        return Category.objects.all()
+    key_value_serializer_class = CategorySerializer
+    queryset = Category.objects.all()
