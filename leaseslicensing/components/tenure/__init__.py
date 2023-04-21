cbf14c4bd3d7cac16870ae1f8f0c36261e608fbc
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-04-21 10:42:24 +0800

    Add more informative comment.

--- leaseslicensing/components/tenure/__init__.py
+++ leaseslicensing/components/tenure/__init__.py
@@ -1,3 +1,7 @@
 """ This component was created to contain models and apis relating to extra information about
 proposals as the proposals component has gotten very large.
+
+The models from this component are grouped together because they are all found on the same
+form in the legacy system "DBCA Tenure". We could potentially rename this in future if it's causing
+confusion
  """
