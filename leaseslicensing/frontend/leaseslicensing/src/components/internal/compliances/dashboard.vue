0ef782b6fceaa0bcf9cc9c69b7a29a326e806aa1
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-04-28 10:32:39 +0800

    Clean up.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/compliances/dashboard.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/compliances/dashboard.vue
@@ -1,31 +1,17 @@
 <template>
     <div class="container" id="compliancesDash">
         <FormSection :formCollapse="false" label="Compliances" Index="compliances">
-            <CompliancesTable
-                level="internal"
-            />
+            <CompliancesTable level="internal" />
         </FormSection>
     </div>
 </template>
 
 <script>
-import FormSection from "@/components/forms/section_toggle.vue"
 import CompliancesTable from "@/components/common/table_compliances.vue"
 export default {
     name: 'InternalCompliancesDashboard',
-    data() {
-        let vm = this;
-        return {
-
-        }
-    },
-    components:{
-        FormSection,
+    components: {
         CompliancesTable,
-    },
-    methods: {
-
     },
 }
 </script>
-
