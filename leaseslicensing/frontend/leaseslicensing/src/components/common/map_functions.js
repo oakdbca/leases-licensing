79b49ff88a61098cb79928ee4cab6238ec41c92b
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-05-01 12:39:24 +0800

    Reset the sketchCoordinates and sketchCoordinatesHistory when the draw mode is turned on.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/map_functions.js
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/map_functions.js
@@ -17,60 +17,60 @@ export var baselayer_name = 'mapbox-emerald'
 export function addOptionalLayers(map_component) {
     let parser = new WMSCapabilities();
 
-    fetch(url).then(function(response) {
+    fetch(url).then(function (response) {
         return response.text();
-        }).then(function(text) {
-            let result = parser.read(text);
-            let layers = result.Capability.Layer.Layer.filter(layer => {return layer['Name'] === 'dbca_legislated_lands_and_waters'});
+    }).then(function (text) {
+        let result = parser.read(text);
+        let layers = result.Capability.Layer.Layer.filter(layer => { return layer['Name'] === 'dbca_legislated_lands_and_waters' });
 
-            for (let j in layers) {
-                let layer = layers[j];
+        for (let j in layers) {
+            let layer = layers[j];
 
-                let l = new TileWMS({
-                    url: `${env['kmi_server_url']}/geoserver/public/wms`,
-                    params: {
-                        'FORMAT': 'image/png',
-                        'VERSION': '1.1.1',
-                        tiled: true,
-                        STYLES: '',
-                        LAYERS: `public:${layer.Name}`
-                    }
-                });
-
-                let tileLayer= new TileLayer({
-                    title: layer.Title.trim(),
-                    visible: false,
-                    source: l,
-                })
-
-                let legend_url = null;
-                if (layer.Name == baselayer_name) {
-                    // TODO don't add the baselayer to the optional layer control
-                } else {
-                    if (typeof(layer.Style) != 'undefined') {
-                        legend_url = layer.Style[0].LegendURL[0].OnlineResource
-                    }
+            let l = new TileWMS({
+                url: `${env['kmi_server_url']}/geoserver/public/wms`,
+                params: {
+                    'FORMAT': 'image/png',
+                    'VERSION': '1.1.1',
+                    tiled: true,
+                    STYLES: '',
+                    LAYERS: `public:${layer.Name}`
                 }
+            });
 
-                // Set additional attributes to the layer
-                tileLayer.set('columns', []) // []
-                tileLayer.set('display_all_columns', true) // true
-                tileLayer.set('legend_url', legend_url);
+            let tileLayer = new TileLayer({
+                title: layer.Title.trim(),
+                visible: false,
+                source: l,
+            })
 
-                map_component.optionalLayers.push(tileLayer)
-                map_component.map.addLayer(tileLayer)
+            let legend_url = null;
+            if (layer.Name == baselayer_name) {
+                // TODO don't add the baselayer to the optional layer control
+            } else {
+                if (typeof (layer.Style) != 'undefined') {
+                    legend_url = layer.Style[0].LegendURL[0].OnlineResource
+                }
+            }
 
-                tileLayer.on("change:visible", function(e){
-                    if (e.oldValue == false) {
-                        $('#legend').find('img').attr('src', this.values_.legend_url)
-                        $('#legend_title').text(this.values_.title);
-                    } else if (e.oldValue == true) {
-                        $('#legend_title').text('');
-                        $('#legend').find('img').attr('src', "")
-                    } else {
-                        console.error("Cannot assess tile layer visibility change.")
-                    }
-                });
+            // Set additional attributes to the layer
+            tileLayer.set('columns', []) // []
+            tileLayer.set('display_all_columns', true) // true
+            tileLayer.set('legend_url', legend_url);
+
+            map_component.optionalLayers.push(tileLayer)
+            map_component.map.addLayer(tileLayer)
+
+            tileLayer.on("change:visible", function (e) {
+                if (e.oldValue == false) {
+                    $('#legend').find('img').attr('src', this.values_.legend_url)
+                    $('#legend_title').text(this.values_.title);
+                } else if (e.oldValue == true) {
+                    $('#legend_title').text('');
+                    $('#legend').find('img').attr('src', "")
+                } else {
+                    console.error("Cannot assess tile layer visibility change.")
+                }
+            });
         }
     });
 }
@@ -82,62 +82,64 @@ export function addOptionalLayers(map_component) {
 export function set_mode(mode) {
     // Toggle map mode on/off when the new mode is the old one
     if (this.mode == mode) {
-        this .mode = 'layer'
+        this.mode = 'layer'
     } else {
-        this .mode = mode
+        this.mode = mode
     }
-    if (this .mode === 'layer'){
-        this .clearMeasurementLayer()
-        _helper.toggle_draw_measure_license.bind(this )(false, false)
-    } else if (this .mode === 'draw') {
-        this .clearMeasurementLayer()
-        _helper.toggle_draw_measure_license.bind(this )(false, true)
-    } else if (this .mode === 'measure') {
-        _helper.toggle_draw_measure_license.bind(this )(true, false)
+    if (this.mode === 'layer') {
+        this.clearMeasurementLayer()
+        _helper.toggle_draw_measure_license.bind(this)(false, false)
+    } else if (this.mode === 'draw') {
+        this.clearMeasurementLayer()
+        this.sketchCoordinates = [[]]
+        this.sketchCoordinatesHistory = [[]]
+        _helper.toggle_draw_measure_license.bind(this)(false, true)
+    } else if (this.mode === 'measure') {
+        _helper.toggle_draw_measure_license.bind(this)(true, false)
     } else {
         console.error(`Cannot set mode ${mode}`)
     }
 }
 
- /**
+/**
 * Defines polygon feature styling depending on whether the polygon source
 * is a Registration of Interest or a Competitive Process
 * @param {object} feature A single geometry feature (e.g. a drawn polygon)
 */
 export function polygon_style(feature) {
-   let strokecolor = "#3498DB";
-   let fillcolor = "#85C1E9"
-   if(feature.get('source') === 'registration_of_interest') {
-       // yellowish
-       strokecolor = [244, 208, 63, 1.0];
-       fillcolor = [241, 196, 15, .7];
-   } else if(feature.get('source') === 'competitive_process') {
-       // orangish
-       strokecolor = [240, 178, 122, 1.0];
-       fillcolor = [230, 126, 34, .7];
-   }
-   return new Style({
-       fill: new Fill({
-           color: fillcolor
-       }),
-       stroke: new Stroke({
-           color: strokecolor,
-           width: 1,
-       }),
-   });
+    let strokecolor = "#3498DB";
+    let fillcolor = "#85C1E9"
+    if (feature.get('source') === 'registration_of_interest') {
+        // yellowish
+        strokecolor = [244, 208, 63, 1.0];
+        fillcolor = [241, 196, 15, .7];
+    } else if (feature.get('source') === 'competitive_process') {
+        // orangish
+        strokecolor = [240, 178, 122, 1.0];
+        fillcolor = [230, 126, 34, .7];
+    }
+    return new Style({
+        fill: new Fill({
+            color: fillcolor
+        }),
+        stroke: new Stroke({
+            color: strokecolor,
+            width: 1,
+        }),
+    });
 }
 
 
 /**
  * Module with map related helper functions
  */
-const _helper =  {
+const _helper = {
     /**
      * Toggles measure and polygon layer active or inactive
      * @param {boolean} drawForMeasure Whether to set the measure layer active or inactive
      * @param {boolean} drawForLeaselicence Whether to set the polygon layer active or inactive
      */
-    toggle_draw_measure_license: function(drawForMeasure, drawForLeaselicence) {
+    toggle_draw_measure_license: function (drawForMeasure, drawForLeaselicence) {
         if (this.drawForMeasure) {
             this.drawForMeasure.setActive(drawForMeasure)
         }
