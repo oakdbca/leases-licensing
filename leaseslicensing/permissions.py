a03fafd2452fd953f162ae906424ec92baa3c725
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-05-18 11:34:47 +0800

    Improve naming of object permission checking method. Add superuser override.

--- leaseslicensing/permissions.py
+++ leaseslicensing/permissions.py
@@ -9,25 +9,27 @@ logger = logging.getLogger(__name__)
 
 class IsInternalAPIView(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
         if is_internal(request):
             if "DELETE" == request.method:
                 return False
 
-            return True
-        if is_customer(request):
             return True
         return False
 
 
 class IsInternalOrHasObjectDocumentsPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
         if is_internal(request):
             return True
         if is_customer(request):
-            if not hasattr(obj, "can_user_view_documents"):
+            if not hasattr(obj, "user_has_object_permission"):
                 raise AttributeError(
-                    f"Object: {obj} must define a can_user_view_documents method to use this permission."
+                    f"Object: {obj} must define a user_has_object_permission method to use this permission."
                 )
-            return obj.can_user_view_documents(request.user.id)
+            return obj.user_has_object_permission(request.user.id)
 
         return False
