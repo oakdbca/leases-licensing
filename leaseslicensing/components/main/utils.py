0e5269fcbd2937ad7685557fa31f8c0b17e1dfb0
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-05-18 13:46:57 +0800

    Added convenience method get_secure_file_url.

--- leaseslicensing/components/main/utils.py
+++ leaseslicensing/components/main/utils.py
@@ -175,7 +175,14 @@ def multipolygon_intersects_with_layer(multipolygon, layer_name):
     return True
 
 
-def get_secure_document_url(instance, related_name, document_id=None):
+def get_secure_file_url(instance, file_field_name):
+    base_path = settings.SECURE_FILE_API_BASE_PATH
+    return (
+        f"{base_path}{instance._meta.model.__name__}/{instance.id}/{file_field_name}/"
+    )
+
+
+def get_secure_document_url(instance, related_name="documents", document_id=None):
     base_path = settings.SECURE_DOCUMENT_API_BASE_PATH
     if document_id:
         return f"{base_path}{instance._meta.model.__name__}/{instance.id}/{related_name}/{document_id}/"
