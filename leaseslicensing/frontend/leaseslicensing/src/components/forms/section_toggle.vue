a79ea0cf8d58ae50e8023b8a2f45b40fcb570712
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-05-23 15:00:40 +0800

    Enabled sub titles on form sections and tweaked existing subtitles for the external dashboard.

--- leaseslicensing/frontend/leaseslicensing/src/components/forms/section_toggle.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/forms/section_toggle.vue
@@ -1,23 +1,14 @@
 <template lang="html">
     <div class="card section-wrapper" :id="custom_id">
         <div class="card-header fw-bold h4" style='padding:30px;'>
-            <div 
-                class='row show_hide_switch' 
-                :id="'show_hide_switch_' + section_body_id" 
-                aria-expanded="true" 
-                :aria-controls="section_body_id"
-                @click="toggle_show_hide"
-            >
+            <div class='row show_hide_switch' :id="'show_hide_switch_' + section_body_id" aria-expanded="true"
+                :aria-controls="section_body_id" @click="toggle_show_hide">
                 <div class='col-11' :style="'color:' + customColor">
-                    {{ label }}
+                    {{ label }} <small v-if="subtitle" class="text-muted">{{ subtitle }}</small>
                 </div>
                 <div class='col-1 text-end'>
                     <!--i :id="chevron_elem_id" class="rotate_icon fa-solid fa-chevron-right"></i-->
-                    <i 
-                        :id="chevron_elem_id" 
-                        class="bi fw-bold chevron-toggle"
-                        :data-bs-target="'#' + section_body_id" 
-                    >
+                    <i :id="chevron_elem_id" class="bi fw-bold chevron-toggle" :data-bs-target="'#' + section_body_id">
                     </i>
                 </div>
             </div>
@@ -34,7 +25,7 @@
 import { v4 as uuid } from 'uuid';
 
 export default {
-    name:"FormSection",
+    name: "FormSection",
     props: {
         label: {},
         subtitle: {
@@ -49,7 +40,7 @@ export default {
             default: false,
         },
     },
-    data:function () {
+    data: function () {
         return {
             custom_id: uuid(),
             chevron_elem_id: 'chevron_elem_' + uuid(),
@@ -68,8 +59,8 @@ export default {
         }
     },
     */
-    computed:{
-        detailsClass: function() {
+    computed: {
+        detailsClass: function () {
             let classText = 'card-body';
             if (this.formCollapse) {
                 classText = 'card-body collapse';
@@ -77,10 +68,10 @@ export default {
             return classText;
         },
         section_header_id: function () {
-            return "section_header_"+this.Index;
+            return "section_header_" + this.Index;
         },
         section_body_id: function () {
-            return "section_body_"+this.Index;
+            return "section_body_" + this.Index;
         },
     },
     methods: {
@@ -92,11 +83,11 @@ export default {
         }
         */
     },
-    mounted: function() {
+    mounted: function () {
         //this.toggle_show_hide()
         chevron_toggle.init()
     },
-    created: function() {
+    created: function () {
     },
 }
 </script>
@@ -106,12 +97,15 @@ export default {
     margin-bottom: 20px;
     padding: 0;
 }
-.show_hide_switch{
+
+.show_hide_switch {
     cursor: pointer;
 }
+
 .rotate_icon {
     transition: 0.5s;
 }
+
 .chev_rotated {
     transform: rotate(90deg);
 }
