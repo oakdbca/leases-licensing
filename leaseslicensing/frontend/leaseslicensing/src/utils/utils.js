0853e5dbfadcf2de1ed308685bfdb9e6f3d17447
Karsten Prehn <karsten.prehn@gmail.com>
2023-06-28 04:09:35 +0000

    Sorted out error response handling in generic fetchUrl function

--- leaseslicensing/frontend/leaseslicensing/src/utils/utils.js
+++ leaseslicensing/frontend/leaseslicensing/src/utils/utils.js
@@ -1,4 +1,8 @@
-import { api_endpoints, helpers } from '@/utils/hooks';
+import {
+    constants,
+    api_endpoints,
+    helpers
+} from '@/utils/hooks';
 
 export default {
     fetchCountries: function () {
@@ -349,23 +353,27 @@ export default {
      * @param {String} url The url endpoint to fetch data from
      * @param {Object} options The options to pass to the fetch function (optional)
      * @returns a Promise
+     * @todo Items for future:
+     * 1) Best practice way to deal with the resolve and reject from calling code.
+     * 2) Does our !response.ok block act appropriately for all possible error response formats that django and drf can respond with.
      */
     fetchUrl: async function (url, options) {
         return new Promise((resolve, reject) => {
-            let f = options === undefined? fetch(url): fetch(url, options);
+            let f = options === undefined ? fetch(url) : fetch(url, options);
             f.then(async response => {
                 const data = await response.json();
                 if (!response.ok) {
                     let error = (data.constructor.name === 'Array' && data) || (data && data.message) || response.statusText;
-                    console.log(error);
+                    console.error(error);
                     reject(error);
                 }
                 resolve(data);
             })
-            .catch(error => {
-                console.error('There was an error!', error);
-                reject(error);
-            });
+                .catch(error => {
+                    console.error(`There was an error fetching from ${url}`, error);
+                    error = new Error(constants.ERRORS.NETWORK_ERROR);
+                    reject(error);
+                });
         });
     }
 };
