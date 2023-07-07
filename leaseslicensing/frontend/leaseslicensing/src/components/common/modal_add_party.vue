46322db05f19b04c552617ff3d7f06bc2169e266
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-07-07 12:11:36 +0800

    New eslintify.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/modal_add_party.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/modal_add_party.vue
@@ -1,14 +1,15 @@
 <template lang="html">
     <div id="modal_add_party">
-        <Modal 
-            ref="modal_add_party" 
-            transition="modal fade" 
-            @ok="okClicked" 
-            @cancel="cancel" 
-            title="Add Party" 
-            okText="Add" large
+        <Modal
+            ref="modal_add_party"
+            transition="modal fade"
+            title="Add Party"
+            ok-text="Add"
+            large
+            :ok-disabled="disableOkButton"
+            @ok="okClicked"
+            @cancel="cancel"
             @mounted="modalMounted"
-            :okDisabled="disableOkButton"
         >
             <div class="container-fluid">
                 <div class="row modal-input-row">
@@ -17,80 +18,109 @@
                     </div>
                     <div class="col-sm-9">
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="person" v-model="type_to_add">
-                            <label class="form-check-label" for="inlineRadio1">Person</label>
+                            <input
+                                id="inlineRadio1"
+                                v-model="type_to_add"
+                                class="form-check-input"
+                                type="radio"
+                                name="inlineRadioOptions"
+                                value="person"
+                            />
+                            <label class="form-check-label" for="inlineRadio1"
+                                >Person</label
+                            >
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="organisation" v-model="type_to_add">
-                            <label class="form-check-label" for="inlineRadio2">Organisation</label>
+                            <input
+                                id="inlineRadio2"
+                                v-model="type_to_add"
+                                class="form-check-input"
+                                type="radio"
+                                name="inlineRadioOptions"
+                                value="organisation"
+                            />
+                            <label class="form-check-label" for="inlineRadio2"
+                                >Organisation</label
+                            >
                         </div>
                     </div>
                 </div>
-                <div v-show="type_to_add=='person'" class="row modal-input-row">
+                <div
+                    v-show="type_to_add == 'person'"
+                    class="row modal-input-row"
+                >
                     <div class="col-sm-3">
                         <label class="form-label">Person</label>
                     </div>
                     <div class="col-sm-7">
-                        <select 
-                            class="form-select" 
+                        <select
+                            id="select_email_users"
+                            ref="email_users"
+                            class="form-select"
                             aria-label="Select person to add"
                             :disabled="false"
-                            ref="email_users"
-                            id="select_email_users"
                         >
                             <option value="null"></option>
-                            <option v-for="user in email_users" :value="user.email" :key="user.id">{{user.name}}</option>
+                            <option
+                                v-for="user in email_users"
+                                :key="user.id"
+                                :value="user.email"
+                            >
+                                {{ user.name }}
+                            </option>
                         </select>
                     </div>
                 </div>
-                <div v-show="type_to_add=='organisation'" class="row modal-input-row">
+                <div
+                    v-show="type_to_add == 'organisation'"
+                    class="row modal-input-row"
+                >
                     <div class="col-sm-3">
                         <label class="form-label">Organisation</label>
                     </div>
                     <div class="col-sm-7">
-                        <select 
-                            class="form-select" 
+                        <select
+                            ref="organisations"
+                            class="form-select"
                             aria-label="Select organisation to add"
                             :disabled="false"
-                            ref="organisations"
                         >
                             <option value="null"></option>
-                            <option v-for="organisation in organisations" :value="organisation.email" :key="organisation.id">{{ organisation.name }}</option>
+                            <option
+                                v-for="organisation in organisations"
+                                :key="organisation.id"
+                                :value="organisation.email"
+                            >
+                                {{ organisation.name }}
+                            </option>
                         </select>
                     </div>
                 </div>
             </div>
-            <!-- <div slot="footer">
-                <button type="button" v-if="saving" disabled class="btn btn-primary" @click="ok"><i class="fa fa-spinner fa-spin"></i> Saving</button>
-                <button type="button" v-else class="btn btn-primary" @click="ok">OK</button>
-                <button type="button" class="btn btn-primary" @click="cancel">Cancel</button>
-            </div> -->
         </Modal>
     </div>
 </template>
 
 <script>
-//import $ from 'jquery'
 import Modal from '@vue-utils/bootstrap-modal.vue'
-import {helpers, api_endpoints} from "@/utils/hooks.js"
+import { api_endpoints } from '@/utils/hooks.js'
 
 export default {
-    name:'Add Party',
-    components:{
+    name: 'AddParty',
+    components: {
         Modal,
     },
-    props:{
-    },
-    data:function () {
-        let vm = this;
+    props: {},
+    emits: ['partyToAdd', 'closeModal'],
+    data: function () {
         return {
-            isModalOpen:false,
+            isModalOpen: false,
             errors: false,
             errorString: '',
             successString: '',
-            success:false,
+            success: false,
             saving: false,
-            
+
             type_to_add: '',
             // Person
             email_users: [],
@@ -102,128 +132,123 @@ export default {
         }
     },
     computed: {
-        showError: function() {
-            var vm = this;
-            return vm.errors;
+        showError: function () {
+            var vm = this
+            return vm.errors
         },
-        disableOkButton: function(){
+        disableOkButton: function () {
             let disabled = true
-            if (this.selected_email_user || this.selected_organisation){
+            if (this.selected_email_user || this.selected_organisation) {
                 disabled = false
             }
             return disabled
         },
     },
-    watch: {
+    watch: {},
+    mounted: function async() {
+        let vm = this
+        vm.$nextTick(async () => {
+            vm.initialiseSelectPerson()
+            vm.initialiseSelectOrganisation()
+        })
     },
-    methods:{
-        modalMounted: function(){
-            console.log("Add-party mounted.")
+    created: async function () {},
+    methods: {
+        modalMounted: function () {
+            console.log('Add-party mounted.')
         },
-        initialiseSelectPerson: function(){
-            let vm = this;
-            $(vm.$refs.email_users).select2({
-                minimumInputLength: 2,
-                "theme": "bootstrap-5",
-                allowClear: true,
-                placeholder:"Type and select Person",
-                // dropdownParent: $('#modal_add_party'),
-                ajax: {
-                    url: api_endpoints.person_lookup,
-                    dataType: 'json',
-                    data: function(params) {
-                        var query = {
-                            term: params.term,
-                            type: 'public',
-                        }
-                        return query;
+        initialiseSelectPerson: function () {
+            let vm = this
+            $(vm.$refs.email_users)
+                .select2({
+                    minimumInputLength: 2,
+                    theme: 'bootstrap-5',
+                    allowClear: true,
+                    placeholder: 'Type and select Person',
+                    // dropdownParent: $('#modal_add_party'),
+                    ajax: {
+                        url: api_endpoints.person_lookup,
+                        dataType: 'json',
+                        data: function (params) {
+                            var query = {
+                                term: params.term,
+                                type: 'public',
+                            }
+                            return query
+                        },
+                        processResults: function (data) {
+                            return data
+                        },
                     },
-                    processResults: function(data){
-                        return data;
-                    }
-                },
-            })
-            .on("select2:select", function (e) {
-                vm.selected_email_user = e.params.data;
-            })
-            .on("select2:unselect",function (e) {
-                var selected = $(e.currentTarget);
-                vm.selected_email_user = null;
-            })
+                })
+                .on('select2:select', function (e) {
+                    vm.selected_email_user = e.params.data
+                })
+                .on('select2:unselect', function () {
+                    vm.selected_email_user = null
+                })
         },
-        initialiseSelectOrganisation: function(){
-            let vm = this;
-            $(vm.$refs.organisations).select2({
-                minimumInputLength: 2,
-                "theme": "bootstrap-5",
-                allowClear: true,
-                placeholder:"Type and select Organisation",
-                ajax: {
-                    url: api_endpoints.organisation_lookup,
-                    dataType: 'json',
-                    data: function(params) {
-                        var query = {
-                            term: params.term,
-                            type: 'public',
-                        }
-                        return query;
+        initialiseSelectOrganisation: function () {
+            let vm = this
+            $(vm.$refs.organisations)
+                .select2({
+                    minimumInputLength: 2,
+                    theme: 'bootstrap-5',
+                    allowClear: true,
+                    placeholder: 'Type and select Organisation',
+                    ajax: {
+                        url: api_endpoints.organisation_lookup,
+                        dataType: 'json',
+                        data: function (params) {
+                            var query = {
+                                term: params.term,
+                                type: 'public',
+                            }
+                            return query
+                        },
+                        processResults: function (data) {
+                            return data
+                        },
                     },
-                    processResults: function(data){
-                        return data;
-                    }
-                },
-            })
-            .on("select2:select", function (e) {
-                let data = e.params.data;
-                vm.selected_organisation = data;
-            })
-            .on("select2:unselect",function (e) {
-                var selected = $(e.currentTarget);
-                vm.selected_organisation = null;
-            })
+                })
+                .on('select2:select', function (e) {
+                    let data = e.params.data
+                    vm.selected_organisation = data
+                })
+                .on('select2:unselect', function () {
+                    vm.selected_organisation = null
+                })
         },
-        okClicked:function () {
+        okClicked: function () {
             let party_to_add = null
-            if (this.type_to_add === 'person'){
+            if (this.type_to_add === 'person') {
                 party_to_add = this.selected_email_user
-            } else if (this.type_to_add === 'organisation'){
+            } else if (this.type_to_add === 'organisation') {
                 party_to_add = this.selected_organisation
             }
-            if (party_to_add){
+            if (party_to_add) {
                 this.$emit('partyToAdd', {
                     // Issue an event with type and person/organisation
-                    'type': this.type_to_add,
-                    'party_to_add': party_to_add
+                    type: this.type_to_add,
+                    party_to_add: party_to_add,
                 })
             }
             this.close()
         },
-        cancel:function () {
+        cancel: function () {
             this.close()
         },
-        close:function () {
+        close: function () {
             this.selected_email_user = null
             this.selected_organisation = null
             $(this.$refs.email_users).empty()
             $(this.$refs.organisations).empty()
-            this.isModalOpen = false;
-            this.errors = false;
-            $('.has-error').removeClass('has-error');
-            this.$emit('closeModal');
+            this.isModalOpen = false
+            this.errors = false
+            $('.has-error').removeClass('has-error')
+            this.$emit('closeModal')
         },
-        addEventListeners:function () {
-
-        },
-    },
-    mounted:function async () {
-        let vm = this
-        vm.$nextTick(async ()=>{
-            vm.initialiseSelectPerson()
-            vm.initialiseSelectOrganisation()
-        });
-    },
-    created: async function() {
-
+        addEventListeners: function () {},
     },
 }
 </script>
