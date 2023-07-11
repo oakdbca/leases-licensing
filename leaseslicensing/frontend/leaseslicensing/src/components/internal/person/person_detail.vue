c2523f64d1884296371937a6a99a2c6bfd49fc6a
Karsten Prehn <karsten.prehn@gmail.com>
2023-07-11 03:51:55 +0000

    Prettyfied/linterized person_detail.vue

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/person/person_detail.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/person/person_detail.vue
@@ -1,44 +1,102 @@
 <template>
-    <div class="container" id="personDash">
+    <div id="personDash" class="container">
         <h3>{{ userHeader }}</h3>
         <div class="row">
             <div class="col-md-3">
-                <CommsLogs :comms_url="comms_url" :logs_url="logs_url" :comms_add_url="comms_add_url"
-                    :disable_add_entry="false" />
+                <CommsLogs
+                    :comms_url="comms_url"
+                    :logs_url="logs_url"
+                    :comms_add_url="comms_add_url"
+                    :disable_add_entry="false"
+                />
             </div>
 
             <div class="col-md-9">
-                <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                <ul id="pills-tab" class="nav nav-pills" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="pills-details-tab" data-toggle="pill" href="#pills-details"
-                            role="tab" aria-controls="pills-details" aria-selected="true">
+                        <a
+                            id="pills-details-tab"
+                            class="nav-link active"
+                            data-toggle="pill"
+                            href="#pills-details"
+                            role="tab"
+                            aria-controls="pills-details"
+                            aria-selected="true"
+                        >
                             Details
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-approvals-tab" data-toggle="pill" href="#pills-approvals" role="tab"
-                            aria-controls="pills-approvals" aria-selected="false">
+                        <a
+                            id="pills-approvals-tab"
+                            class="nav-link"
+                            data-toggle="pill"
+                            href="#pills-approvals"
+                            role="tab"
+                            aria-controls="pills-approvals"
+                            aria-selected="false"
+                        >
                             Approvals
                         </a>
                     </li>
                 </ul>
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane show active" id="pills-details" role="tabpanel"
-                        aria-labelledby="pills-details-tab">
-                        <Applicant v-if="user" :email-user="user" id="proposalStartApplicant" :readonly="readonly" />
+                <div id="pills-tabContent" class="tab-content">
+                    <div
+                        id="pills-details"
+                        class="tab-pane show active"
+                        role="tabpanel"
+                        aria-labelledby="pills-details-tab"
+                    >
+                        <Applicant
+                            v-if="user"
+                            id="proposalStartApplicant"
+                            :email-user="user"
+                            :readonly="readonly"
+                        />
                     </div>
-                    <div class="tab-pane fade" id="pills-approvals" role="tabpanel" aria-labelledby="pills-approvals-tab">
-                        <FormSection :formCollapse="true" label="Applications" subtitle="" index="applications">
-                            <ApplicationsTable v-if="user" level="organisation_view" :target_id="user.id" />
+                    <div
+                        id="pills-approvals"
+                        class="tab-pane fade"
+                        role="tabpanel"
+                        aria-labelledby="pills-approvals-tab"
+                    >
+                        <FormSection
+                            :form-collapse="true"
+                            label="Applications"
+                            subtitle=""
+                            index="applications"
+                        >
+                            <ApplicationsTable
+                                v-if="user"
+                                level="organisation_view"
+                                :target_id="user.id"
+                            />
                         </FormSection>
 
-                        <FormSection :formCollapse="true" label="Approvals" subtitle="" index="approvals">
-                            <AppprovalsTable v-if="user" level="organisation_view" :target_id="user.id" />
+                        <FormSection
+                            :form-collapse="true"
+                            label="Approvals"
+                            subtitle=""
+                            index="approvals"
+                        >
+                            <AppprovalsTable
+                                v-if="user"
+                                level="organisation_view"
+                                :target_id="user.id"
+                            />
                         </FormSection>
 
-                        <FormSection :formCollapse="true" label="Compliances with Requirements" subtitle=""
-                            index="compliances">
-                            <CompliancesTable v-if="user" level="internal" :target_id="user.id" />
+                        <FormSection
+                            :form-collapse="true"
+                            label="Compliances with Requirements"
+                            subtitle=""
+                            index="compliances"
+                        >
+                            <CompliancesTable
+                                v-if="user"
+                                level="internal"
+                                :target_id="user.id"
+                            />
                         </FormSection>
                     </div>
                 </div>
@@ -48,29 +106,16 @@
 </template>
 
 <script>
-import FormSection from "@/components/forms/section_toggle.vue"
-import Applicant from '@/components/common/applicant.vue'
-import ApplicationsTable from "@/components/common/table_proposals"
-import AppprovalsTable from "@/components/common/table_approvals"
-import CompliancesTable from "@/components/common/table_compliances"
-import { api_endpoints, helpers, constants } from '@/utils/hooks'
-import CommsLogs from '@common-utils/comms_logs.vue'
+import FormSection from '@/components/forms/section_toggle.vue';
+import Applicant from '@/components/common/applicant.vue';
+import ApplicationsTable from '@/components/common/table_proposals';
+import AppprovalsTable from '@/components/common/table_approvals';
+import CompliancesTable from '@/components/common/table_compliances';
+import { api_endpoints, helpers, constants } from '@/utils/hooks';
+import CommsLogs from '@common-utils/comms_logs.vue';
 
 export default {
     name: 'PersonDetail',
-    data() {
-        let vm = this
-        return {
-            user: null,
-            errorMessage: null,
-            allApprovalTypeFilter: ['ml', 'aap', 'aup'],
-            wlaApprovalTypeFilter: ['wla',],
-
-            comms_url: helpers.add_endpoint_json(api_endpoints.users, vm.$route.params.id + '/comms_log'),
-            comms_add_url: helpers.add_endpoint_json(api_endpoints.users, vm.$route.params.id + '/add_comms_log'),
-            logs_url: helpers.add_endpoint_json(api_endpoints.users, vm.$route.params.id + '/action_log'),
-        }
-    },
     components: {
         FormSection,
         Applicant,
@@ -79,56 +124,88 @@ export default {
         CompliancesTable,
         CommsLogs,
     },
+    data() {
+        let vm = this;
+        return {
+            user: null,
+            errorMessage: null,
+            allApprovalTypeFilter: ['ml', 'aap', 'aup'],
+            wlaApprovalTypeFilter: ['wla'],
+
+            comms_url: helpers.add_endpoint_json(
+                api_endpoints.users,
+                vm.$route.params.id + '/comms_log'
+            ),
+            comms_add_url: helpers.add_endpoint_json(
+                api_endpoints.users,
+                vm.$route.params.id + '/add_comms_log'
+            ),
+            logs_url: helpers.add_endpoint_json(
+                api_endpoints.users,
+                vm.$route.params.id + '/action_log'
+            ),
+        };
+    },
     computed: {
         readonly: function () {
-            return true
+            return true;
         },
         userHeader: function () {
             if (this.user) {
                 if (this.user.dob) {
-                    return this.user.first_name + ' ' + this.user.last_name + '(DOB: ' + this.user.dob + ')'
+                    return (
+                        this.user.first_name +
+                        ' ' +
+                        this.user.last_name +
+                        '(DOB: ' +
+                        this.user.dob +
+                        ')'
+                    );
                 } else {
-                    return this.user.first_name + ' ' + this.user.last_name
+                    return this.user.first_name + ' ' + this.user.last_name;
                 }
             }
-            return ''
-        }
-    },
-    methods: {
-        fetchUser: function (id) {
-            let vm = this;
-            fetch(api_endpoints.users + id + '/')
-                .then(async response => {
-                    const data = await response.json();
-                    if (!response.ok) {
-                        const error = (data && data.message) || response.statusText;
-                        console.log(error)
-                        return Promise.reject(error);
-                    }
-                    vm.user = data
-                    console.log(vm.user)
-                })
-                .catch(error => {
-                    this.errorMessage = constants.ERRORS.API_ERROR;
-                    console.error("There was an error!", error);
-                });
+            return '';
         },
     },
     created: async function () {
-        console.log(this.$route.params.id)
+        console.log(this.$route.params.id);
         this.fetchUser(this.$route.params.id);
     },
     mounted: function () {
-        var triggerTabList = [].slice.call(document.querySelectorAll('#pills-tab a'));
+        var triggerTabList = [].slice.call(
+            document.querySelectorAll('#pills-tab a')
+        );
         triggerTabList.forEach(function (triggerEl) {
-            var tabTrigger = new bootstrap.Tab(triggerEl)
+            var tabTrigger = new bootstrap.Tab(triggerEl);
             triggerEl.addEventListener('click', function (event) {
                 event.preventDefault();
                 tabTrigger.show();
             });
         });
     },
-}
+    methods: {
+        fetchUser: function (id) {
+            let vm = this;
+            fetch(api_endpoints.users + id + '/')
+                .then(async (response) => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText;
+                        console.log(error);
+                        return Promise.reject(error);
+                    }
+                    vm.user = data;
+                    console.log(vm.user);
+                })
+                .catch((error) => {
+                    this.errorMessage = constants.ERRORS.API_ERROR;
+                    console.error('There was an error!', error);
+                });
+        },
+    },
+};
 </script>
 
 <style scoped>
@@ -149,20 +226,20 @@ export default {
     margin-bottom: 2px;
 }
 
-.nav-item>li>a {
+.nav-item > li > a {
     background-color: yellow !important;
     color: #fff;
 }
 
-.nav-item>li.active>a,
-.nav-item>li.active>a:hover,
-.nav-item>li.active>a:focus {
+.nav-item > li.active > a,
+.nav-item > li.active > a:hover,
+.nav-item > li.active > a:focus {
     color: white;
     background-color: blue;
     border: 1px solid #888888;
 }
 
-.admin>div {
+.admin > div {
     display: inline-block;
     vertical-align: top;
     margin-right: 1em;
