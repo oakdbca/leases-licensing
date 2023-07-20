a6a766ad6c2fe7c3490046947c832c9131355234
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-07-20 16:04:31 +0800

    Made component add financial year rows if there is no backend data but not overwrite rows if existing backend data does not exist.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/component_percentage_turnover.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/component_percentage_turnover.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="row mb-3">
         <div class="col">
-            <template v-for="year in financialYearsArray" :key="year.id">
+            <template
+                v-for="year in grossTurnoverPercentagesComputed"
+                :key="year.year"
+            >
                 <div class="card mb-2">
                     <div class="card-body py-1">
                         <div class="div d-flex align-items-center">
@@ -11,7 +14,7 @@
                                         >Financial Year</span
                                     >
                                     <input
-                                        v-model="year.year"
+                                        v-model="year.financial_year"
                                         type="text"
                                         readonly
                                         class="form-control form-control-year"
@@ -39,6 +42,11 @@
                                         v-model="year.gross_turnover"
                                         type="number"
                                         class="form-control"
+                                        :readonly="
+                                            grossTurnoverReadonly(
+                                                year.financial_year
+                                            )
+                                        "
                                     />
                                     <span class="input-group-text">AUD</span>
                                 </div>
@@ -52,15 +60,15 @@
     <div class="row">
         <div class="col">
             <BootstrapAlert class="py-2 mb-0">
-                The approval holder will be asked to upload an audited financial
-                statement for each financial year.
+                Compliances will be automatically generated to ask for an
+                audited financial statement for each financial year
             </BootstrapAlert>
         </div>
     </div>
 </template>
 
 <script>
-import { helpers } from '@/utils/hooks'
+import { constants, helpers } from '@/utils/hooks'
 
 export default {
     name: 'PercentageTurnover',
@@ -73,28 +81,76 @@ export default {
             type: String,
             required: true,
         },
+        grossTurnoverPercentages: {
+            type: Array,
+            required: true,
+        },
+        proposalProcessingStatusId: {
+            type: String,
+            required: true,
+        },
     },
+    emits: ['updateGrossTurnoverPercentages'],
     data: function () {
         return {
-            financialYearsArray: this.getFinancialYearsArray(),
+            financialYearHasPassed: helpers.financialYearHasPassed,
+        }
+    },
+    computed: {
+        grossTurnoverPercentagesComputed: {
+            get: function () {
+                return this.grossTurnoverPercentages
+            },
+            set: function (value) {
+                this.$emit('updateGrossTurnoverPercentages', value)
+            },
+        },
+    },
+    created: function () {
+        const financialYearsIncluded = helpers.financialYearsIncluded(
+            this.startDate,
+            this.expiryDate
+        )
+        if (
+            this.grossTurnoverPercentages.length !=
+            financialYearsIncluded.length
+        ) {
+            this.populateFinancialYearsArray(financialYearsIncluded)
         }
     },
     methods: {
-        getFinancialYearsArray: function () {
-            const financialYearsIncluded = helpers.financialYearsIncluded(
-                this.startDate,
-                this.expiryDate
+        grossTurnoverReadonly: function (financialYear) {
+            console.log(financialYear)
+            // Gross turnover is readonly if the financial year hasn't passed
+            // or if the proposal is being edited from the proposal details page
+            return (
+                !this.financialYearHasPassed(financialYear) ||
+                constants.PROPOSAL_STATUS.APPROVED_EDITING_INVOICING.ID ==
+                    this.proposalProcessingStatusId
             )
-            const records = []
+        },
+        populateFinancialYearsArray: function (financialYearsIncluded) {
+            var financialYears = []
+
             for (let i = 0; i < financialYearsIncluded.length; i++) {
-                records.push({
-                    id: 0,
-                    year: financialYearsIncluded[i],
-                    percentage: 0.0,
-                    gross_turnover: null,
-                })
+                let year = financialYearsIncluded[i].split('-')[1]
+                if (
+                    this.grossTurnoverPercentagesComputed.filter(
+                        (x) => x.year == year
+                    ).length == 0
+                ) {
+                    financialYears.push({
+                        id: 0,
+                        year: year,
+                        financial_year: financialYearsIncluded[i],
+                        percentage: 0.0,
+                        gross_turnover: null,
+                    })
+                }
             }
-            return records
+            this.grossTurnoverPercentagesComputed =
+                this.grossTurnoverPercentagesComputed.concat(...financialYears)
+            console.log(this.grossTurnoverPercentagesComputed)
         },
     },
 }
