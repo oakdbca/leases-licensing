05417f603d772f3d524bc7c9d3e0228e4ab45ed7
Karsten Prehn <karsten.prehn@gmail.com>
2023-08-01 04:31:44 +0000

    Starting work on approval history with a nicely linterized approval_history.vue

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/approvals/approval_history.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/approvals/approval_history.vue
@@ -1,16 +1,28 @@
 <template lang="html">
     <div id="approvalHistory">
-        <modal transition="modal fade" @cancel="close()" :title="'History for Approval ' + approvalLodgementNumber"
-            :extraLarge="true" cancelText="Close" okText="">
+        <modal
+            transition="modal fade"
+            :title="'History for Approval ' + approvalLodgementNumber"
+            :extra-large="true"
+            cancel-text="Close"
+            ok-text=""
+            @cancel="close()"
+        >
             <div class="container-fluid">
                 <div class="row">
-                    <alert v-if="errorString" type="danger"><strong>{{ errorString }}</strong></alert>
+                    <alert v-if="errorString" type="danger"
+                        ><strong>{{ errorString }}</strong></alert
+                    >
                     <div class="col-sm-12">
                         <div class="form-group">
                             <div class="row">
                                 <div v-if="approvalId" class="col-lg-12">
-                                    <datatable ref="history_datatable" :id="datatable_id" :dtOptions="datatable_options"
-                                        :dtHeaders="datatable_headers" />
+                                    <datatable
+                                        :id="datatable_id"
+                                        ref="history_datatable"
+                                        :dt-options="datatable_options"
+                                        :dt-headers="datatable_headers"
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -21,13 +33,13 @@
     </div>
 </template>
 <script>
-import modal from '@vue-utils/bootstrap-modal.vue'
-import alert from '@vue-utils/alert.vue'
-import { helpers, api_endpoints, constants } from "@/utils/hooks.js"
-import datatable from '@/utils/vue/datatable.vue'
+import modal from '@vue-utils/bootstrap-modal.vue';
+import alert from '@vue-utils/alert.vue';
+import { helpers, api_endpoints, constants } from '@/utils/hooks.js';
+import datatable from '@/utils/vue/datatable.vue';
 
 export default {
-    name: 'approvalHistory',
+    name: 'ApprovalHistory',
     components: {
         modal,
         alert,
@@ -53,123 +65,132 @@ export default {
             isModalOpen: false,
             messageDetails: '',
             ccEmail: '',
-            isModalOpen: false,
             validation_form: null,
             errorString: '',
             successString: '',
             success: false,
-        }
+        };
     },
     computed: {
         csrf_token: function () {
-            return helpers.getCookie('csrftoken')
+            return helpers.getCookie('csrftoken');
         },
         datatable_headers: function () {
-            return ['id', 'Number', 'Type', 'Holder', 'Application', 'Reason', 'Expiry Date', 'Document', 'Action']
+            return [
+                'id',
+                'Number',
+                'Type',
+                'Holder',
+                'Application',
+                'Reason',
+                'Expiry Date',
+                'Document',
+                'Action',
+            ];
         },
         column_id: function () {
             return {
                 // 1. ID
-                data: "id",
+                data: 'id',
                 orderable: false,
                 searchable: false,
                 visible: false,
-                'render': function (row, type, full) {
-                    return full.id
-                }
-            }
+                render: function (row, type, full) {
+                    return full.id;
+                },
+            };
         },
         column_lodgement_number: function () {
             return {
                 // 2. Lodgement Number
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
-                    return full.approval_lodgement_number
+                render: function (row, type, full) {
+                    return full.approval_lodgement_number;
                 },
-            }
+            };
         },
         column_type: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return full.approval_type_description;
-                }
-            }
+                },
+            };
         },
         column_sticker_numbers: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return full.sticker_numbers;
-                }
-            }
+                },
+            };
         },
         column_holder: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return full.holder;
-                }
-            }
+                },
+            };
         },
         column_approval_status: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return full.approval_status;
-                }
-            }
+                },
+            };
         },
         column_start_date: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
-                    return full.start_date_str
-                }
-            }
+                render: function (row, type, full) {
+                    return full.start_date_str;
+                },
+            };
         },
         column_reason: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return full.reason;
-                }
-            }
+                },
+            };
         },
         column_approval_letter: function () {
             return {
-                data: "id",
+                data: 'id',
                 orderable: true,
                 searchable: true,
                 visible: true,
-                'render': function (row, type, full) {
+                render: function (row, type, full) {
                     return `<div><a href='${full.approval_letter}' target='_blank'><i style='color:red;' class='fa fa-file-pdf-o'></i></a></div>`;
-                }
-            }
+                },
+            };
         },
         datatable_options: function () {
-            let vm = this
+            let vm = this;
             let columns = [
                 vm.column_id,
                 vm.column_lodgement_number,
@@ -180,7 +201,7 @@ export default {
                 vm.column_start_date,
                 vm.column_reason,
                 vm.column_approval_letter,
-            ]
+            ];
 
             return {
                 autoWidth: false,
@@ -192,22 +213,25 @@ export default {
                 ordering: true,
                 order: [[0, 'desc']],
                 ajax: {
-                    "url": api_endpoints.lookupApprovalHistory(this.approvalId),
-                    "dataSrc": 'data',
+                    url: api_endpoints.lookupApprovalHistory(this.approvalId),
+                    dataSrc: 'data',
 
                     // adding extra GET params for Custom filtering
-                    "data": function (d) {
-                    }
+                    // eslint-disable-next-line no-unused-vars
+                    data: function (d) {},
                 },
                 dom: 'lBfrtip',
                 buttons: [],
                 columns: columns,
                 processing: true,
                 initComplete: function () {
-                    console.log('in initComplete')
+                    console.log('in initComplete');
                 },
-            }
-        }
+            };
+        },
+    },
+    created: function () {
+        this.fetchApprovalDetails();
     },
     methods: {
         close: function () {
@@ -216,14 +240,13 @@ export default {
             $('.has-error').removeClass('has-error');
         },
         fetchApprovalDetails: async function () {
-            const res = await fetch(api_endpoints.lookupApprovalDetails(this.approvalId));
+            const res = await fetch(
+                api_endpoints.lookupApprovalDetails(this.approvalId)
+            );
             if (res.ok) {
                 this.approvalDetails = Object.assign({}, res.body);
             }
         },
     },
-    created: function () {
-        this.fetchApprovalDetails();
-    },
-}
+};
 </script>
