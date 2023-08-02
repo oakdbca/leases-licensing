af454bdd16f9cb2f4545c018d0018310ab370edc
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-02 16:54:04 +0800

    Eslintify and add dropdownParent for select2.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/proposals/proposed_approval_documents.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/proposals/proposed_approval_documents.vue
@@ -97,10 +97,10 @@
 </template>
 
 <script>
-import { constants } from '@/utils/hooks'
-import { helpers, api_endpoints } from '@/utils/hooks.js'
-import FileField from '@/components/forms/filefield_immediate.vue'
-import { updateIdListFromAvailable } from '@/components/common/workflow_functions.js'
+import { constants } from '@/utils/hooks';
+import { helpers, api_endpoints } from '@/utils/hooks.js';
+import FileField from '@/components/forms/filefield_immediate.vue';
+import { updateIdListFromAvailable } from '@/components/common/workflow_functions.js';
 
 export default {
     name: 'ProposedApprovalDocuments',
@@ -133,7 +133,7 @@ export default {
         return {
             selectedDocumentTypes: [],
             availableDocumentTypes: [],
-        }
+        };
     },
     computed: {
         withApprover: function () {
@@ -141,22 +141,22 @@ export default {
                 this.proposal.processing_status_id
             )
                 ? true
-                : false
+                : false;
         },
         isLeaseLicence: function () {
             if (
                 this.proposal &&
                 this.proposal.application_type.name === 'lease_licence'
             ) {
-                return true
+                return true;
             }
-            return false
+            return false;
         },
         leaseLicenceApprovalDocumentsUrl: function () {
             return helpers.add_endpoint_join(
                 api_endpoints.proposal,
                 this.proposal.id + '/process_lease_licence_approval_document/'
-            )
+            );
         },
         canChangeDocuments: function () {
             /** The assessor and the approver can change the documents
@@ -166,51 +166,52 @@ export default {
                 constants.PROPOSAL_STATUS.WITH_ASSESSOR.ID,
                 constants.PROPOSAL_STATUS.WITH_ASSESSOR_CONDITIONS.ID,
                 constants.PROPOSAL_STATUS.WITH_APPROVER.ID,
-            ].includes(this.proposal.processing_status_id)
+            ].includes(this.proposal.processing_status_id);
         },
         selectedDocumentTypesIds: function () {
             // Return the ids of selected document types from the document type-dropdown
-            return this.selectedDocumentTypes.map(({ id }) => id)
+            return this.selectedDocumentTypes.map(({ id }) => id);
         },
     },
     created: async function () {
-        let vm = this
-        vm.approval = Object.assign({}, vm.proposal.proposed_issuance_approval)
-        vm.availableDocumentTypes = []
-        vm.selectedDocumentTypes = []
+        let vm = this;
+        vm.approval = Object.assign({}, vm.proposal.proposed_issuance_approval);
+        vm.availableDocumentTypes = [];
+        vm.selectedDocumentTypes = [];
 
         vm.$nextTick(() => {
-            vm.initSelectDocument()
+            vm.initSelectDocument();
 
             // Available Document Types
             for (const approvalType of vm.approvalTypes) {
                 if (approvalType.id === vm.selectedApprovalTypeId) {
                     for (const docType of approvalType.approval_type_document_types) {
-                        vm.availableDocumentTypes.push(docType)
+                        vm.availableDocumentTypes.push(docType);
                     }
                 }
             }
 
             // Selected Document Types
             if (vm.approval.selected_document_types) {
-                vm.selectedDocumentTypes = vm.approval.selected_document_types
+                vm.selectedDocumentTypes = vm.approval.selected_document_types;
             }
-        })
+        });
     },
     methods: {
         documentTypeSelectorBlur() {
             $('#' + 'documentTypeSelector')
                 .val(null)
-                .trigger('change')
+                .trigger('change');
         },
         /**
          * Initialise the select2 control for adding documents as part of the application
          */
         initSelectDocument: function () {
-            let vm = this
+            let vm = this;
 
             $(vm.$refs.select_document)
                 .select2({
+                    dropdownParent: $('#proposedIssuanceApproval .modal'),
                     theme: 'bootstrap-5',
                     allowClear: true,
                     placeholder: 'Add a document',
@@ -222,14 +223,14 @@ export default {
                             if (vm.selectedDocumentTypes.length == 0) {
                                 return $(
                                     '<div style="color: grey;">Add a document</div>'
-                                )
+                                );
                             } else {
                                 return $(
                                     '<div style="color: grey;">Add another document</div>'
-                                )
+                                );
                             }
                         }
-                        return $('<div">' + data.text + '</div>')
+                        return $('<div">' + data.text + '</div>');
                     },
                     templateResult: function (result) {
                         // Add custom styling to the selection dropdown options
@@ -239,42 +240,42 @@ export default {
                                 '<div style="color: grey;">' +
                                     result.text +
                                     '</div>'
-                            )
+                            );
                         } else {
-                            return $('<div>' + result.text + '</div>')
+                            return $('<div>' + result.text + '</div>');
                         }
                     },
                 })
                 .on('select2:select', function (e) {
-                    var selected = $(e.currentTarget)
+                    var selected = $(e.currentTarget);
                     // Check if user can add documents
                     if (vm.canChangeDocuments) {
                         let added = vm.updateSelectedDocumentTypes(
                             selected.val()
-                        )
+                        );
                         if (added) {
                             // If a new item has been added, clear the select2 field. If an existing
                             // item has been selected, show it as selected so it can be deleted.
-                            console.log('added')
-                            vm.documentTypeSelectorBlur()
+                            console.log('added');
+                            vm.documentTypeSelectorBlur();
                         }
                     }
                 })
                 .on('select2:unselecting', function () {
-                    var self = $(this)
+                    var self = $(this);
                     setTimeout(() => {
-                        self.select2('close')
-                    }, 0)
+                        self.select2('close');
+                    }, 0);
                 })
                 .on('select2:unselect', function (e) {
                     // Check if user can change documents
                     if (vm.canChangeDocuments) {
-                        let unselected_id = e.params.data.id
+                        let unselected_id = e.params.data.id;
                         // Remove the unselected item from the list of selected items
-                        vm.updateSelectedDocumentTypes(unselected_id, true)
-                        vm.documentTypeSelectorBlur()
+                        vm.updateSelectedDocumentTypes(unselected_id, true);
+                        vm.documentTypeSelectorBlur();
                     }
-                })
+                });
         },
         /**
          * Update selected items from multi-select document type-dropdown.
@@ -287,13 +288,13 @@ export default {
                 this.selectedDocumentTypes,
                 this.availableDocumentTypes,
                 remove
-            )
+            );
             if (list) {
-                this.selectedDocumentTypes = list
+                this.selectedDocumentTypes = list;
             } else {
-                return false
+                return false;
             }
         },
     },
-}
+};
 </script>
