9710ea1243f5c6db4018bc8f1ab4ccc1c18f9b79
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-15 17:00:02 +0800

    Rewrite to suit new requirements.

--- leaseslicensing/management/commands/generate_invoices.py
+++ leaseslicensing/management/commands/generate_invoices.py
@@ -1,21 +1,14 @@
 import logging
-from datetime import timedelta
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.utils import timezone
-from ledger_api_client import utils as ledger_api_client_utils
 
 from leaseslicensing.components.approvals.models import Approval
 from leaseslicensing.components.invoicing.email import (
     send_new_invoice_raised_notification,
 )
-from leaseslicensing.components.invoicing.models import Invoice, InvoicingAndReviewDates
-from leaseslicensing.components.organisations.models import (
-    Organisation,
-    OrganisationContact,
-)
+from leaseslicensing.components.invoicing.models import Invoice
 from leaseslicensing.components.proposals.models import Proposal
 
 logger = logging.getLogger(__name__)
@@ -35,18 +28,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        year = timezone.now().year
-        date = timezone.now().date()
-        month = timezone.now().month
-        month_name = timezone.now().strftime("%B")
-        day_of_month = timezone.now().day
-
-        try:
-            invoicing_and_review_dates = InvoicingAndReviewDates.objects.get(year=year)
-        except InvoicingAndReviewDates.DoesNotExist:
-            logger.error(f"Unable to retrieve InvoicingAndReviewDates for year: {year}")
-            return
-
         approvals = Approval.objects.filter(
             status=Approval.APPROVAL_STATUS_CURRENT,
             current_proposal__processing_status=Proposal.PROCESSING_STATUS_APPROVED,
@@ -63,174 +44,41 @@ class Command(BaseCommand):
             ]
         )
 
-        if options["test"]:
-            logger.info("Running in test mode - no invoices will be generated")
-            self.generate_annual_invoices(approvals, test=True)
-            self.generate_quarterly_invoices(approvals, test=True)
-            self.generate_monthly_invoices(approvals, test=True)
-            return
+        for approval in approvals:
+            if (
+                not hasattr(approval, "invoicing_details")
+                or approval.invoicing_details is None
+            ):
+                logger.warn(f"\n\nNo invoicing details found for Approval: {approval}")
+                return
 
-        if date == invoicing_and_review_dates.invoicing_date_annually:
-            logger.info(f"Today is the annual invoicing date for {year}.")
-            self.generate_annual_invoices(approvals)
+            invoicing_details = approval.invoicing_details
 
-        if (
-            month in [3, 6, 9, 12]
-            and day_of_month == invoicing_and_review_dates.invoicing_day_for_quarter
-        ):
-            logger.info(
-                f"Today is the quarterly invoicing date for the {month_name} quarter of {year}."
+            if not invoicing_details.has_future_invoicing_periods:
+                logger.info(
+                    f"Skipping Approval: {approval} - no future invoicing periods"
+                )
+                continue
+
+            invoices_due_for_issue_today = (
+                invoicing_details.invoices_due_for_issue_today
             )
-            self.generate_quarterly_invoices(approvals)
-
-        if day_of_month == invoicing_and_review_dates.invoicing_day_for_month:
-            logger.info(
-                f"Today is the monthly invoicing date for {month_name}, {year}."
-            )
-            self.generate_monthly_invoices(approvals)
-
-    def generate_annual_invoices(self, approvals, test=False):
-        annual_invoicing_approvals = approvals.filter(
-            current_proposal__invoicing_details__invoicing_repetition_type__key=settings.REPETITION_TYPE_ANNUALLY
-        )
-        logger.info(
-            f"\n\nFound {annual_invoicing_approvals.count()} approvals that need annual invoices generated.\n"
-        )
-        for approval in annual_invoicing_approvals:
-            self.generate_invoice(approval, test=test)
-
-    def generate_quarterly_invoices(self, approvals, test=False):
-        quarterly_invoicing_approvals = approvals.filter(
-            current_proposal__invoicing_details__invoicing_repetition_type__key=settings.REPETITION_TYPE_QUARTERLY
-        )
-        logger.info(
-            f"\n\nFound {quarterly_invoicing_approvals.count()} approvals that need quarterly invoices generated.\n"
-        )
-        for approval in quarterly_invoicing_approvals:
-            self.generate_invoice(approval, test=test)
-
-    def generate_monthly_invoices(self, approvals, test=False):
-        logger.info("\n\nGenerating monthly invoices\n")
-        monthly_invoicing_approvals = approvals.filter(
-            current_proposal__invoicing_details__invoicing_repetition_type__key=settings.REPETITION_TYPE_MONTHLY
-        )
-        logger.info(
-            f"\n\nFound {monthly_invoicing_approvals.count()} approvals that need monthly invoices generated.\n"
-        )
-        for approval in monthly_invoicing_approvals:
-            self.generate_invoice(approval, test=test)
+            for invoice in invoices_due_for_issue_today:
+                self.generate_invoice(approval, invoice, test=options["test"])
 
     @transaction.atomic
-    def generate_invoice(self, approval, test=False):
-        if (
-            not hasattr(approval.current_proposal, "invoicing_details")
-            or approval.current_proposal.invoicing_details is None
-        ):
-            logger.warn(f"\n\nNo invoicing details found for Approval: {approval}")
-            return
-
-        invoicing_details = approval.current_proposal.invoicing_details
-
-        # Todo: For custom CPI based invoices make sure the cpi has been entered
-        # If not email finance officer?
-        if not invoicing_details.ready_for_invoicing:
-            return
-
-        invoice_amount = invoicing_details.invoice_amount
-        # Todo add the invoicing frequency to the description
-        description = f"{approval.approval_type} {approval.lodgement_number}"
-        gst_free = approval.approval_type.gst_free
-        due_date = timezone.now().date() + timedelta(
-            days=settings.DEFAULT_DAYS_BEFORE_PAYMENT_DUE
-        )
-
+    def generate_invoice(self, approval, invoice, test=False):
         invoice = Invoice(
             approval=approval,
-            amount=invoice_amount,
-            gst_free=gst_free,
-            date_due=due_date,
+            amount=invoice["amount_object"]["amount"],
+            gst_free=approval.approval_type.gst_free,
         )
 
         if test:
-            logger.info(f"\n\nTest mode - Invoice would be generated: {invoice}")
-            return
-
-        invoice.save()
-
-        ledger_order_lines = []
-        ledger_order_lines.append(
-            {
-                "ledger_description": description,
-                "quantity": 1,
-                "price_excl_tax": str(
-                    invoice_amount
-                ),  # Todo gst applies for leases but not for licences
-                "price_incl_tax": str(invoice_amount),
-                "oracle_code": "Todo: Get Oracle Code",
-                "line_status": settings.LEDGER_DEFAULT_LINE_STATUS,
-            },
-        )
-
-        request = ledger_api_client_utils.FakeRequestSessionObj()
-
-        basket_params = {
-            "products": ledger_order_lines,
-            "vouchers": [],
-            "system": settings.PAYMENT_SYSTEM_ID,
-            "tax_override": True,
-            "custom_basket": True,
-            "booking_reference": str(invoice.lodgement_number),
-            "no_payment": True,
-        }
-        if type(approval.applicant) == Organisation:
-            organisation = approval.applicant
-            basket_params["organisation"] = organisation.ledger_organisation_id
-            admin_contact = organisation.contacts.filter(
-                user_role=OrganisationContact.USER_ROLE_CHOICE_ADMIN,
-                user_status=OrganisationContact.USER_STATUS_CHOICE_ACTIVE,
-            ).first()
-            if not admin_contact:
-                logger.error(
-                    f"Unable to retrieve admin contact for organisation: {organisation}"
-                )
-                return
-
-            request.user = admin_contact.user
-        else:
-            request.user = approval.applicant
-
-        basket_hash = ledger_api_client_utils.create_basket_session(
-            request, request.user.id, basket_params
-        )
-        basket_hash = basket_hash.split("|")[0]
-
-        invoice_text = f"Leases Licensing Invoice {invoice.lodgement_number}"
-        if approval.current_proposal.proponent_reference_number:
-            invoice_text += f"(Proponent Ref: {approval.current_proposal.proponent_reference_number})"
-
-        return_preload_url = (
-            f"{settings.LEASES_LICENSING_EXTERNAL_URL}"
-            f"/api/invoicing/ledger-api-invoice-success-callback/{invoice.uuid}"
-        )
-
-        future_invoice = ledger_api_client_utils.process_create_future_invoice(
-            basket_hash, invoice_text, return_preload_url
-        )
-
-        logger.info(f"Future Invoice: {future_invoice}")
-
-        if 200 != future_invoice["status"]:
-            logger.error(
-                f"Failed to create future Invoice {invoice.lodgement_number} with basket_hash "
-                f"{basket_hash}, invoice_text {invoice_text}, return_preload_url {return_preload_url}"
+            logger.info(
+                f"Test mode - Invoice record would be generated: {invoice} (and finance group would be sent an email)"
             )
             return
-
-        data = future_invoice["data"]
-
-        invoice.order_number = data["order"]
-        invoice.basket_id = data["basket_id"]
-        invoice.invoice_reference = data["invoice"]
 
         invoice.save()
 
