1fbb2b508535ed4d6a6b1e828d0bc183553051bc
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-16 15:00:11 +0800

    Increase max_digits for amount on invoices.

--- leaseslicensing/components/invoicing/models.py
+++ leaseslicensing/components/invoicing/models.py
@@ -1263,7 +1263,7 @@ class Invoice(LicensingModel):
         null=True,
         blank=True,
     )
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     gst_free = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True, null=False)
     date_updated = models.DateTimeField(auto_now=True, null=False)
