9a9a590daa00510d46877073b82d17f6db190637
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-17 12:05:00 +0800

    Remove unused management commands.

--- leaseslicensing/management/commands/monthly_invoices.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Run the Monthly Invoices Script - generates invoices per licence/org_applicant for previous month"
-
-    def handle(self, *args, **options):
-        raise NotImplementedError("This script is not yet implemented")
