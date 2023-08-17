7bf5d2e8807cd75628d3f8897afc7663a347dee3
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-17 16:42:11 +0800

    Change application type filter and field on approvals datatable to be approval type which makes much more sense as being useful.

--- leaseslicensing/components/approvals/serializers.py
+++ leaseslicensing/components/approvals/serializers.py
@@ -88,60 +88,6 @@ class ApprovalPaymentSerializer(serializers.ModelSerializer):
     #    return obj.current_proposal_id
 
 
-class _ApprovalPaymentSerializer(serializers.ModelSerializer):
-    applicant = serializers.SerializerMethodField(read_only=True)
-    applicant_type = serializers.SerializerMethodField(read_only=True)
-    applicant_id = serializers.SerializerMethodField(read_only=True)
-    status = serializers.CharField(source="get_status_display")
-    title = serializers.CharField(source="current_proposal.title")
-    application_type = serializers.SerializerMethodField(read_only=True)
-    land_parks = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Approval
-        fields = (
-            "id",
-            "lodgement_number",
-            "current_proposal",
-            "title",
-            "issue_date",
-            "start_date",
-            "expiry_date",
-            "applicant",
-            "applicant_type",
-            "applicant_id",
-            "status",
-            "cancellation_date",
-            "application_type",
-            "land_parks",
-        )
-
-    def get_application_type(self, obj):
-        if obj.current_proposal.application_type:
-            return obj.current_proposal.application_type.name
-        return None
-
-    def get_applicant(self, obj):
-        return (
-            obj.applicant.name
-            if isinstance(obj.applicant, Organisation)
-            else obj.applicant
-        )
-
-    def get_applicant_type(self, obj):
-        return obj.applicant_type
-
-    def get_applicant_id(self, obj):
-        return obj.applicant_id
-
-    def get_land_parks(self, obj):
-        return None  # obj.current_proposal.land_parks
-        # return AuthorSerializer(obj.author).data
-        # if obj.current_proposal.land_parks:
-        #    return ProposalParkSerializer(obj.current_proposal.land_parks).data
-        # return None
-
-
 class ApprovalSerializer(serializers.ModelSerializer):
     applicant_type = serializers.SerializerMethodField(read_only=True)
     applicant_id = serializers.SerializerMethodField(read_only=True)
@@ -173,8 +119,7 @@ class ApprovalSerializer(serializers.ModelSerializer):
     current_proposal_processing_status = serializers.CharField(
         source="current_proposal.processing_status", read_only=True
     )
-    approval_type = serializers.SerializerMethodField(read_only=True)
-    approval_type_obj = serializers.SerializerMethodField(read_only=True)
+    approval_type = serializers.CharField(source="approval_type.name", read_only=True)
     gis_data = serializers.SerializerMethodField(read_only=True)
     geometry_objs = serializers.SerializerMethodField(read_only=True)
 
@@ -226,7 +171,6 @@ class ApprovalSerializer(serializers.ModelSerializer):
             "record_management_number",
             "invoicing_details",
             "approval_type",
-            "approval_type_obj",
             "gis_data",
             "geometry_objs",
         )
@@ -333,29 +277,6 @@ class ApprovalSerializer(serializers.ModelSerializer):
 
     def get_groups_comma_list(self, obj):
         return obj.current_proposal.groups_comma_list
-
-    def get_approval_type(self, obj):
-        approval_type_obj = self.get_approval_type_obj(obj)
-        if approval_type_obj is None:
-            return None
-        return approval_type_obj.get("name", None)
-
-    def get_approval_type_obj(self, obj):
-        if not obj.current_proposal.proposed_issuance_approval:
-            logger.debug("No approval issuance proposed yet")
-            return None
-        approval_type_id = obj.current_proposal.proposed_issuance_approval.get(
-            "approval_type", None
-        )
-        if approval_type_id is None:
-            logger.warn("ApprovalType not found")
-            return None
-        try:
-            approval_type = ApprovalType.objects.get(id=approval_type_id)
-        except ApprovalType.DoesNotExist:
-            return None
-        else:
-            return ApprovalTypeSerializer(approval_type).data
 
     def get_gis_data(self, obj):
         return ProposalGisDataSerializer(obj.current_proposal).data
