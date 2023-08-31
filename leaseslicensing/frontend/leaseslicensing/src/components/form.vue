a6943fb55a0de62fc73cb9a651c6486d977fe5b9
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-31 16:56:37 +0800

    Emit refresh from response from map component for when a shapefile is uploaded.

--- leaseslicensing/frontend/leaseslicensing/src/components/form.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/form.vue
@@ -119,6 +119,7 @@
                                     : 'Use the <b>draw</b> tool to draw the area of the proposal you are interested in on the map.</br>Display layers to check attributes of polygons with the <b>info</b> tool.</br>You can <b>save</b> the application and continue at a later time.'
                             "
                             @validate-feature="validateFeature.bind(this)()"
+                            @refreshFromResponse="refreshFromResponse"
                         />
                     </FormSection>
                 </div>
