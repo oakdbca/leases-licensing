9a224d5e23010838a76a97e426e484b3710fb1d6
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-09-07 11:13:00 +0800

    Further configuration of component.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/invoices/invoice_generate_ad_hoc_record.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/invoices/invoice_generate_ad_hoc_record.vue
@@ -4,6 +4,7 @@
             transition="modal fade"
             title="Generate Ad Hoc Invoice Record"
             ok-text="Generate Invoice Record"
+            :ok-disabled="generatingAdHocInvoiceRecord"
             cancel-text="Cancel"
             large
             @ok="validateForm"
@@ -16,6 +17,14 @@
                             class="needs-validation"
                             novalidate
                         >
+                            <BootstrapAlert
+                                v-if="errors"
+                                class="alert alert-danger mb-0"
+                                role="alert"
+                                icon="exclamation-triangle-fill"
+                            >
+                                <ErrorRenderer :errors="errors" />
+                            </BootstrapAlert>
                             <div class="row mt-3 mb-3">
                                 <label
                                     for="approval"
@@ -24,21 +33,30 @@
                                 >
                                 <div class="col-sm-8">
                                     <Multiselect
+                                        v-if="approvals"
+                                        ref="approval"
                                         v-model="invoice.approval"
-                                        label="lodgement_number"
-                                        track-by="id"
+                                        :custom-label="
+                                            (opt) =>
+                                                approvals.find(
+                                                    (x) => x.id == opt
+                                                ).lodgement_number
+                                        "
                                         placeholder="Select Approval"
-                                        :options="approvals"
+                                        :min-chars="2"
+                                        :options="
+                                            approvals.map(
+                                                (approval) => approval.id
+                                            )
+                                        "
+                                        :allow-empty="false"
                                         :hide-selected="true"
                                         :multiple="false"
                                         :searchable="true"
                                         :loading="loadingApprovals"
-                                        required
+                                        :required="true"
+                                        @change="updateFieldRequired"
                                     />
-                                    <div class="invalid-feedback">
-                                        Please select an approval to raise the
-                                        invoice against.
-                                    </div>
                                 </div>
                             </div>
                             <div class="row mt-3 mb-3">
@@ -50,6 +68,7 @@
                                 <div class="col-sm-8">
                                     <input
                                         id="amount"
+                                        v-model="invoice.amount"
                                         type="number"
                                         class="form-control"
                                         step="0.01"
@@ -69,6 +88,7 @@
                                 <div class="col-sm-8">
                                     <textarea
                                         id="description"
+                                        v-model="invoice.description"
                                         class="form-control"
                                         required
                                     ></textarea>
@@ -86,9 +106,11 @@
                                 <div class="col-sm-8">
                                     <input
                                         id="issue-date"
+                                        v-model="invoice.date_issued"
                                         class="form-control"
                                         type="date"
                                         required
+                                        @change="updateDueDate"
                                     />
                                     <div class="invalid-feedback">
                                         Please enter an issue date.
@@ -97,13 +119,14 @@
                             </div>
                             <div class="row mt-3 mb-3">
                                 <label
-                                    for="issue-date"
+                                    for="due-date"
                                     class="col-sm-4 col-form-label"
                                     >Due Date</label
                                 >
                                 <div class="col-sm-8">
                                     <input
-                                        id="issue-date"
+                                        id="due-date"
+                                        v-model="invoice.date_due"
                                         class="form-control"
                                         type="date"
                                         required
@@ -122,6 +145,7 @@
                                 <div class="col-sm-8">
                                     <input
                                         id="oracle-invoice-number"
+                                        v-model="invoice.oracle_invoice_number"
                                         type="text"
                                         class="form-control"
                                         required
@@ -152,6 +176,15 @@
                                         (.pdf)
                                     </div>
                                 </div>
+                                <div
+                                    v-if="generatingAdHocInvoiceRecord"
+                                    class="col-sm-8"
+                                >
+                                    <BootstrapSpinner
+                                        class="text-primary"
+                                        :center-of-screen="false"
+                                    />
+                                </div>
                             </div>
                         </form>
                     </div>
@@ -166,30 +199,31 @@ import modal from '@vue-utils/bootstrap-modal.vue';
 import { api_endpoints } from '@/utils/hooks.js';
 import Swal from 'sweetalert2';
 import Multiselect from 'vue-multiselect';
+import ErrorRenderer from '@common-utils/ErrorRenderer.vue';
 
 export default {
     name: 'InvoiceRaiseAdHoc',
     components: {
+        ErrorRenderer,
         modal,
         Multiselect,
     },
-    emits: ['oracleInvoiceNumberUploaded'],
+    emits: ['adHocInvoiceRecordGenerated'],
     data: function () {
         return {
             isModalOpen: false,
             invoice: {},
             loadingApprovals: false,
+            generatingAdHocInvoiceRecord: false,
             approvals: [],
+            errors: null,
         };
     },
     watch: {
         isModalOpen: function (val) {
             if (val) {
                 this.$nextTick(() => {
-                    $('#generate-ad-hoc-invoice-record-form')
-                        .find('input')
-                        .first()
-                        .focus();
+                    this.focusFirstField();
                 });
             }
         },
@@ -198,8 +232,41 @@ export default {
         this.fetchApprovals();
     },
     methods: {
+        focusFirstField: function () {
+            $('#generate-ad-hoc-invoice-record-form')
+                .find('input')
+                .first()
+                .focus();
+        },
+        updateDueDate: function () {
+            if (this.invoice.date_due) {
+                return;
+            }
+            let date_issued = new Date(this.invoice.date_issued);
+            let date_due = date_issued.setDate(date_issued.getDate() + 30);
+            this.invoice.date_due = new Date(date_due)
+                .toISOString()
+                .split('T')[0];
+        },
         close: function () {
+            var form = document.getElementById(
+                'generate-ad-hoc-invoice-record-form'
+            );
+            form.classList.remove('was-validated');
+            this.errors = null;
             this.isModalOpen = false;
+        },
+        updateFieldRequired() {
+            this.$nextTick(() => {
+                const required =
+                    this.required &&
+                    (this.option == null ||
+                        (Array.isArray(this.option) &&
+                            this.option.length == 0 &&
+                            this.options.length > 0));
+                this.$refs.approval.$el.querySelector('input').required =
+                    required;
+            });
         },
         confirmUpload: function () {
             Swal.fire({
@@ -225,6 +292,11 @@ export default {
                 'generate-ad-hoc-invoice-record-form'
             );
 
+            if (!this.invoice.approval) {
+                this.focusFirstField();
+                return;
+            }
+
             let oracleInvoice = document.getElementById('oracle-invoice');
             if (
                 !oracleInvoice.files[0] ||
@@ -249,7 +321,50 @@ export default {
 
             return false;
         },
-        generateAdHocInvoiceRecord: function () {},
+        generateAdHocInvoiceRecord: function () {
+            let vm = this;
+            let oracleInvoice = $('#oracle-invoice').prop('files')[0];
+            const formData = new FormData();
+            for (let key in this.invoice) {
+                formData.append(key, this.invoice[key]);
+            }
+            formData.append('invoice_pdf', oracleInvoice);
+            const requestOptions = {
+                method: 'POST',
+                body: formData,
+            };
+            vm.generatingAdHocInvoiceRecord = true;
+            fetch(
+                api_endpoints.invoices + 'generate_ad_hoc_invoice/',
+                requestOptions
+            )
+                .then(async (response) => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText;
+                        console.log(error);
+                        vm.errors = data;
+                        return;
+                    }
+                    vm.$emit('adHocInvoiceRecordGenerated', data);
+                    Swal.fire({
+                        title: 'Ad Hoc Invoice Record Generated',
+                        text: `The ad hoc invoice record has been generated.`,
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000,
+                    });
+                    vm.close();
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error);
+                    Promise.reject(error);
+                })
+                .finally(() => {
+                    vm.generatingAdHocInvoiceRecord = false;
+                });
+        },
         fetchApprovals: async function () {
             let vm = this;
             vm.loadingApprovals = true;
@@ -260,14 +375,12 @@ export default {
                     if (!response.ok) {
                         const error =
                             (data && data.message) || response.statusText;
-                        console.log(error);
-                        Promise.reject(error);
+                        console.error(error);
                     }
                     vm.approvals = data;
                 })
                 .catch((error) => {
                     console.error('There was an error!', error);
-                    Promise.reject(error);
                 })
                 .finally(() => {
                     vm.loadingApprovals = false;
