f7d49ad8739763d41627e6778c299d64c92df88f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-09-12 14:20:57 +0800

    Add headings for tourism proposal details sections.

--- leaseslicensing/frontend/leaseslicensing/src/components/form_lease_licence.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/form_lease_licence.vue
@@ -11,7 +11,10 @@
                 name="slot_proposal_tourism_details_assessment_comments"
             ></slot>
 
-            <div class="row mb-3 pt-3">
+            <div class="row mb-2">
+                <div class="fw-bold">Profit and Loss</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="profit_and_loss_text" class="form-label"
                         >Provide five-year integrated profit and loss, balance
@@ -54,7 +57,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Cashflow</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="cash_flow_text" class="form-label"
                         >Provide cash flow projections to demonstrate time to
@@ -92,7 +98,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Capital Investment</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="capital_investment_text" class="form-label"
                         >Include proposed capital investment showing costing
@@ -133,7 +142,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Financial Capacity</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="financial_capacity_text" class="form-label"
                         >Demonstrate that you and/or the nominated operator have
@@ -179,7 +191,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Available Activities</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="available_activities_text" class="form-label"
                         >Specify the full range of recreational, educational,
@@ -223,7 +238,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Market Analysis</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="market_analysis_text" class="form-label"
                         >Outline a market analysis that identifies the target
@@ -267,7 +285,10 @@
                 </div>
             </div>
 
-            <div class="row mb-3 pt-3 border-top">
+            <div class="row mb-2 pt-3 border-top">
+                <div class="fw-bold">Staffing</div>
+            </div>
+            <div class="row mb-3">
                 <div class="col">
                     <label for="staffing_text" class="form-label"
                         >Provide details of staffing, workforce management.
