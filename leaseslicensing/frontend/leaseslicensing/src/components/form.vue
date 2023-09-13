0062e01dd053a1221e13e11a58e6e51bf54e4107
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-09-13 15:10:21 +0800

    Show optional messaging for deed poll for ROI proposals and mandatory messaging for LL proposals.

--- leaseslicensing/frontend/leaseslicensing/src/components/form.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/form.vue
@@ -251,23 +251,48 @@
                         </div>
                     </FormSection>
 
-                    <FormSection label="Deed Poll" index="deed_poll">
+                    <FormSection
+                        label="Deed Poll"
+                        :subtitle="leaseLicence ? 'Mandatory' : 'Optional'"
+                        index="deed_poll"
+                    >
                         <slot name="slot_deed_poll_assessment_comments"></slot>
                         <div class="col-sm-12 section-style">
-                            <p>
-                                <strong
-                                    >It is a requirement of all lease and
-                                    licence holders to sign a deed poll to
-                                    release and indemnify the State of Western
-                                    Australia. Please note: electronic or
-                                    digital signatures cannot be accepted.
-                                    <p></p>
-                                    The deed poll must be signed and have a
-                                    witness signature and be dated. Once signed
-                                    and dated, please scan and attach the deed
-                                    poll below.
-                                </strong>
-                            </p>
+                            <BootstrapAlert
+                                :type="leaseLicence ? 'warning' : 'primary'"
+                                :icon="
+                                    leaseLicence
+                                        ? 'exclamation-triangle-fill'
+                                        : 'info-fill'
+                                "
+                            >
+                                <ul class="list-group">
+                                    <li class="list-group-item">
+                                        <template v-if="leaseLicence">
+                                            It is a requirement of all
+                                            proponents
+                                        </template>
+                                        <template v-else>You may wish</template>
+                                        to sign a deed poll to release and
+                                        indemnify the State of Western
+                                        Australia.
+                                    </li>
+                                    <li class="list-group-item">
+                                        The deed poll must be signed, dated and
+                                        have a witness signature.
+                                    </li>
+                                    <li class="list-group-item">
+                                        Once signed, dated and witnessed, please
+                                        scan and attach the deed poll below.
+                                    </li>
+                                </ul>
+                            </BootstrapAlert>
+
+                            <BootstrapAlert type="danger">
+                                <span class="fw-bold">Please note:</span>
+                                electronic or digital signatures cannot be
+                                accepted.
+                            </BootstrapAlert>
 
                             <label for="deed_poll_document" class="mb-3"
                                 >Deed poll:</label
