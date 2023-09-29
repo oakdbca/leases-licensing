d122e9c3339f0e9fff82947098c89367e5d445d0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-09-29 10:16:59 +0800

    Correct comment.

--- leaseslicensing/components/proposals/api.py
+++ leaseslicensing/components/proposals/api.py
@@ -2315,7 +2315,7 @@ class ProposalStandardRequirementViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = ProposalStandardRequirement.objects.filter(
             application_type__id=application_type_id
         )
-        # Don't show gross turnover related requirement on the front end
+        # Don't show gross turnover related requirements on the front end
         # as they are managed by the system automatically based on the invoicing
         # method that is selected
         queryset.exclude(gross_turnover_required=True)
