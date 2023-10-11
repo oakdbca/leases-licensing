34e63c7de703d6118e9ae102c356af7a7a0c10b0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-11 13:54:36 +0800

    Update fixture path for default standard requirements.

--- leaseslicensing/migrations/0272_auto_20230915_1218.py
+++ leaseslicensing/migrations/0272_auto_20230915_1218.py
@@ -3,7 +3,7 @@
 from django.core.management import call_command
 from django.db import migrations
 
-fixture = "default_standard_requirements"
+fixture = "leaseslicensing/components/proposals/fixtures/default_standard_requirements"
 
 def load_fixture(apps, schema_editor):
     call_command('loaddata', fixture, app_label='leaseslicensing')
