939061953ff51d247b32191e227e98bd35af9264
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-11-17 11:51:34 +0800

    Bug fix: Dev ops 4283: 'Lodged to' filter in Map not working. (Front end filter condition was incorrect.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/component_map_with_filters_v2.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/component_map_with_filters_v2.vue
@@ -1326,7 +1326,7 @@ export default {
                 this.filteredProposals = [
                     ...this.filteredProposals.filter(
                         (proposal) =>
-                            new Date(proposal.lodgement_date) >=
+                            new Date(proposal.lodgement_date) <=
                             new Date(this.filterApplicationsMapLodgedTo)
                     ),
                 ];
