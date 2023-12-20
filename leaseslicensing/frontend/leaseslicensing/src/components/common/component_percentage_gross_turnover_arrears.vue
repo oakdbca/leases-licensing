dfed8c10e6dd64ccef5affb48854c970e9269a6b
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-12-20 16:54:14 +0800

    Add previous condition for gross turnover being disabled back in (had modified it for testing purposed.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/component_percentage_gross_turnover_arrears.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/component_percentage_gross_turnover_arrears.vue
@@ -62,6 +62,9 @@
                                         class="form-control"
                                         :disabled="
                                             year.locked ||
+                                            !financialYearHasPassed(
+                                                year.financial_year
+                                            ) ||
                                             (!yearBeforeIssueDate(year) &&
                                                 !allQuartersLocked(year))
                                         "
