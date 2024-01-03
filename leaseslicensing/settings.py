c2e35dfa990f91ff41fa41e3c2806c6bbf8af915
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-01-03 15:47:25 +0800

    Add explanation for why we set the FILE_UPLOAD_PERMISSIONS to None.

--- leaseslicensing/settings.py
+++ leaseslicensing/settings.py
@@ -232,6 +232,9 @@ PROTECTED_MEDIA_ROOT = env(
 )
 SECURE_FILE_API_BASE_PATH = "/api/main/secure_file/"
 SECURE_DOCUMENT_API_BASE_PATH = "/api/main/secure_document/"
+
+# This is needed so that the chmod is not called in django/core/files/storage.py (line 302)
+# As it causes an permission exception when using azure network drives
 FILE_UPLOAD_PERMISSIONS = None
 
 BASE_URL = env("BASE_URL")
