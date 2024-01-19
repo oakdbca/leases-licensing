cf5629ca3c30c743c4c730ae72727293797320b0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-01-19 11:33:14 +0800

    Add in missing column.

--- leaseslicensing/frontend/leaseslicensing/src/components/common/table_invoices.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/table_invoices.vue
@@ -508,6 +508,7 @@ export default {
                 this.approvalTypeColumn,
                 this.holderColumn,
                 this.statusColumn,
+                this.ledgerInvoicePDFColumn,
                 this.amountColumn,
                 this.incGSTColumn,
                 this.dateDueColumn,
