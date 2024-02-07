c71f95070aa1411029df2133c89c108a4e79eecc
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-07 16:26:22 +0800

    Restrict DetailsTextViewSet to assessors.

--- leaseslicensing/components/texts/api.py
+++ leaseslicensing/components/texts/api.py
@@ -5,16 +5,18 @@ from rest_framework import viewsets
 from leaseslicensing.components.main.api import KeyValueListMixin
 from leaseslicensing.components.texts.models import DetailsText
 from leaseslicensing.components.texts.serializers import DetailsTextSerializer
+from leaseslicensing.permissions import IsAssessor
 
 logger = logging.getLogger(__name__)
 
 
 class DetailsTextViewSet(
     viewsets.ModelViewSet,
-    KeyValueListMixin,  # NoPaginationListMixin, Select2ListMixin
+    KeyValueListMixin,
 ):
     model = DetailsText
     serializer_class = DetailsTextSerializer
     key_value_display_field = "target"
     key_value_serializer_class = DetailsTextSerializer
     queryset = DetailsText.objects.all()
+    permission_classes = [IsAssessor]
