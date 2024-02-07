3356e99dab61c6a4332ef8aa113dee7abf05f922
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-07 10:03:57 +0800

    Remove unused function and lint file.

--- leaseslicensing/frontend/leaseslicensing/src/components/external/utils.js
+++ leaseslicensing/frontend/leaseslicensing/src/components/external/utils.js
@@ -1,48 +1,41 @@
-import api from './api'
-import {helpers} from '@/utils/hooks'
+import api from './api';
+import { helpers } from '@/utils/hooks';
 
 export default {
-    fetchProfile: function (){
-        return new Promise ((resolve,reject) => {
-            fetch(api.profile).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
-        });
-
-    },
-    fetchProposal: function(id){
-        return new Promise ((resolve,reject) => {
-            fetch(helpers.add_endpoint_json(api.proposals,id)).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
+    fetchProfile: function () {
+        return new Promise((resolve, reject) => {
+            fetch(api.profile).then(
+                (response) => {
+                    resolve(response.body);
+                },
+                (error) => {
+                    reject(error);
+                }
+            );
         });
     },
-    fetchCountries: function (){
-        return new Promise ((resolve,reject) => {
-            fetch(api.countries).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
-        });
-
-    },
-
-    fetchOrganisation: function(id){
-        return new Promise ((resolve,reject) => {
-            fetch(helpers.add_endpoint_json(api.organisations,id)).then((response) => {
-                resolve(response.body);
-            },
-            (error) => {
-                reject(error);
-            });
+    fetchCountries: function () {
+        return new Promise((resolve, reject) => {
+            fetch(api.countries).then(
+                (response) => {
+                    resolve(response.body);
+                },
+                (error) => {
+                    reject(error);
+                }
+            );
         });
     },
-}
+    fetchOrganisation: function (id) {
+        return new Promise((resolve, reject) => {
+            fetch(helpers.add_endpoint_json(api.organisations, id)).then(
+                (response) => {
+                    resolve(response.body);
+                },
+                (error) => {
+                    reject(error);
+                }
+            );
+        });
+    },
+};
