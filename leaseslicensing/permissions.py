3c893859f9a37eb95bea71df27d40adecb942ad8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-07 11:33:41 +0800

    Add has_permission method to HasObjectPermission to prevent list view for those users.

--- leaseslicensing/permissions.py
+++ leaseslicensing/permissions.py
@@ -38,6 +38,9 @@ class IsFinanceOfficer(BasePermission):
 
 
 class HasObjectPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
     def has_object_permission(self, request, view, obj):
         if request.user.is_superuser:
             return True
