af1e27509f08a4cef0cab289afa70a0e4572c562
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-01-08 10:38:11 +0800

    Modify exports format.

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/api.js
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/api.js
@@ -1,6 +1,5 @@
-module.exports = {
+export default {
     organisations: '/api/organisations.json/',
-    //countries: 'https://restcountries.eu/rest/v1/?fullText=true',
     users: '/api/users.json',
     profile: '/api/profile',
-}
+};
