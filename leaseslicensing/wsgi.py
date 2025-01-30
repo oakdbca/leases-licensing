4ffdfb81b47cc62331e9fcbe28b10af957b47984
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-01-30 15:25:39 +0800

    Only read .env file if it exists.

--- leaseslicensing/wsgi.py
+++ leaseslicensing/wsgi.py
@@ -3,14 +3,18 @@ WSGI configuration for leases licensing project.
 
 Exposes the WSGI callable as a module-level variable named 'application'
 """
+
 import os
 
 import confy
 from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-confy.read_environment_file(BASE_DIR + "/.env")
-os.environ.setdefault("BASE_DIR", BASE_DIR)
 
+if os.path.exists(BASE_DIR + "/.env"):
+    confy.read_environment_file(BASE_DIR + "/.env")
+
+os.environ.setdefault("BASE_DIR", BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leaseslicensing.settings")
+
 application = get_wsgi_application()
