af2b1a8b03bc4a4cd91968543cf5ae81ebe893b6
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-06-19 09:17:54 +0800

    Update manage.sh to clarify usage requirements for local development

--- manage.sh
+++ manage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Convenience script for local development usage only to cut down on typing,
+# poetry must be installed and the virtual environment must be activated.
+
 poetry run python manage.py $@
